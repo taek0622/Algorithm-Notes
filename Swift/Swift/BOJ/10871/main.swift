dd7e7cb769e29746efe07f43e4686c75c5273a4a
태깅 <min4644@gmail.com>
2022-05-19 16:38:16 +0900

    feat: Swift BOJ 10871

--- Swift/Swift/BOJ/10871/main.swift
+++ Swift/Swift/BOJ/10871/main.swift
@@ -5,16 +5,24 @@
 //  Created by 김민택 on 2022/05/17.
 //
 
-var input = readLine()!.split(separator: " ").compactMap {
-    Int(String($0))!
-}
+// 메모리: 70012KB, 시간: 16ms, 코드 길이: 225B
+var input = readLine()!.split(separator: " ").compactMap { Int(String($0))! }[1]
 
-var intArray = readLine()!.split(separator: " ").compactMap {
-    Int(String($0))!
-}
+print(readLine()!.split(separator: " ").compactMap { Int(String($0))! }.filter { $0 < input }.compactMap { String($0) }.joined(separator: " "))
 
-for i in 0..<input[0] {
-    if input[1] > intArray[i] {
-        print("\(intArray[i])", terminator: " ")
-    }
-}
+/*
+ 메모리: 70012KB, 시간: 20ms, 코드 길이: 281B
+ var input = readLine()!.split(separator: " ").compactMap {
+     Int(String($0))!
+ }
+
+ var intArray = readLine()!.split(separator: " ").compactMap {
+     Int(String($0))!
+ }
+
+ for i in 0..<input[0] {
+     if input[1] > intArray[i] {
+         print("\(intArray[i])", terminator: " ")
+     }
+ }
+ */
