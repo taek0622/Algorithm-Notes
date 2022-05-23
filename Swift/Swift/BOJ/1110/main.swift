f92a94fcdda2ca31478a1948f69668d457a3f14f
태깅 <min4644@gmail.com>
2022-05-24 01:06:17 +0900

    Feat: Swift BOJ 1110

--- Swift/Swift/BOJ/1110/main.swift
+++ Swift/Swift/BOJ/1110/main.swift
@@ -6,3 +6,23 @@
 //
 
 
+
+/*
+ 메모리: 69100KB, 시간: 8ms, 코드 길이: 280B
+ var input = Int(readLine()!)!
+ var tempTen = 0
+ var tempOne = 0
+ var temp = input
+ var cnt = 0
+
+ while true {
+     tempTen = temp / 10
+     tempOne = temp % 10
+     temp = tempOne * 10 + (tempTen + tempOne) % 10
+     cnt += 1
+     if input == temp {
+         print(cnt)
+         break
+     }
+ }
+ */
