37cfae555541112e2e21e5e0212f8d54bca428ae
태깅 <min4644@gmail.com>
2022-06-06 04:45:12 +0900

    Feat: Swift BOJ 11654

---
+++ Swift/Swift/BOJ/11654/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Swift
+//
+//  Created by 김민택 on 2022/06/05.
+//
+
+// 메모리: 69096KB, 시간: 8ms, 코드 길이: 37B
+print(readLine()!.first!.asciiValue!)
+
+/*
+ 메모리: 69096KB, 시간: 8ms, 코드 길이: 45B
+ print(Int(UnicodeScalar(readLine()!)!.value))
+ */
+
+/*
+ 메모리: 69096KB, 시간: 12ms, 코드 길이: 83B
+ let input = readLine()!
+
+ let ascii = Int(UnicodeScalar(input)!.value)
+
+ print(ascii)
+ */
