a69ab56a346863a9611ccf7e6994f352eca0364f
taek0622 <min4644@gmail.com>
2023-01-02 02:23:59 +0900

    Feat:sparkles:: Add BOJ2566 new Solution

--- Swift/Swift-Algorithm/BOJ/9. 2차원 배열/2566.swift
+++ Swift/Swift-Algorithm/BOJ/9. 2차원 배열/2566.swift
@@ -9,7 +9,11 @@
 
 class BOJ2566: Solvable {
     func run() {
-        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 312B
+        solution3()
+    }
+
+    // 메모리: 69104KB, 시간: 8ms, 코드 길이: 312B
+    private func solution1() {
         var maxNum = 0
         var maxIndex = (1, 1)
 
@@ -23,5 +27,37 @@ class BOJ2566: Solvable {
         }
 
         print("\(maxNum)\n\(maxIndex.0) \(maxIndex.1)")
+    }
+
+    // 메모리: 69104KB, 시간: 8ms, 코드 길이: 286B
+    private func solution2() {
+        var maxNum = (0, 1, 1)
+
+        for line in 1...9 {
+            let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+            if input.max()! > maxNum.0 {
+                maxNum = (input.max()!, line, input.firstIndex(of: input.max()!)! + 1)
+            }
+        }
+
+        print("\(maxNum.0)\n\(maxNum.1) \(maxNum.2)")
+    }
+
+    // Apahfl: 69104KB, 시간: 8ms, 코드 길이: 311B
+    private func solution3() {
+        var (maxNum, row, column) = (0, 1, 1)
+
+        for line in 1...9 {
+            let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+            if input.max()! > maxNum {
+                maxNum = input.max()!
+                row = line
+                column = input.firstIndex(of: maxNum)! + 1
+            }
+        }
+
+        print("\(maxNum)\n\(row) \(column)")
     }
 }
