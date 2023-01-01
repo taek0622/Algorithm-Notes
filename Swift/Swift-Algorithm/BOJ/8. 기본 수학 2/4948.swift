2d9fd1c1a76a908727bc82fc94546c1e98967148
taek0622 <min4644@gmail.com>
2023-01-01 23:20:03 +0900

    Feat:sparkles:: Add BOJ4948 solution2

--- Swift/Swift-Algorithm/BOJ/8. 기본 수학 2/4948.swift
+++ Swift/Swift-Algorithm/BOJ/8. 기본 수학 2/4948.swift
@@ -9,15 +9,16 @@
 
 class BOJ4948: Solvable {
     func run() {
-        let maxNum = 123456
-        var numbers = [false, false] + Array(repeating: true, count: maxNum * 2 - 1)
+        solution1()
+    }
 
-        for idx in 2...maxNum*2 where numbers[idx] {
-            if (idx * idx) > (maxNum * 2) {
-                break
-            }
+    // 메모리: 69588KB, 시간: 20ms, 코드 길이: 534B
+    private func solution1() {
+        let maxNum = 123456 * 2
+        var numbers = [false, false] + Array(repeating: true, count: maxNum - 1)
 
-            for num in idx...maxNum*2/idx {
+        for idx in 2...maxNum where numbers[idx] && !((idx * idx) > maxNum) {
+            for num in idx...maxNum/idx {
                 numbers[idx * num] = false
             }
         }
@@ -28,10 +29,41 @@ class BOJ4948: Solvable {
             if n != 0 {
                 var count = 0
 
-                for idx in n+1...n+n {
-                    if numbers[idx] {
-                        count += 1
-                    }
+                for idx in n+1...n+n where numbers[idx] {
+                    count += 1
+                }
+
+                print(count)
+
+                checkBertrandsPostulate(Int(readLine()!)!)
+            }
+        }
+    }
+
+    // 메모리: 69588KB, 시간: 20ms, 코드 길이: 589B
+    private func solution2() {
+        let maxNum = 123456 * 2
+        var numbers = [false, false] + Array(repeating: true, count: maxNum - 1)
+        var idx = 2
+
+        while ((idx * idx) <= maxNum) {
+            if numbers[idx] {
+                for num in stride(from: idx * idx, through: maxNum, by: idx) {
+                    numbers[num] = false
+                }
+            }
+
+            idx += 1
+        }
+
+        checkBertrandsPostulate(Int(readLine()!)!)
+
+        func checkBertrandsPostulate(_ n: Int) {
+            if n != 0 {
+                var count = 0
+
+                for idx in n+1...n+n where numbers[idx] {
+                    count += 1
                 }
 
                 print(count)
