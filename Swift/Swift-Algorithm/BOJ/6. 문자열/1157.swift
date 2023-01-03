bf753108ad3e73e645967a4425cc7a797e06a764
taek0622 <min4644@gmail.com>
2023-01-03 19:38:57 +0900

    Chore:wrench:: Modify BOJ1157

--- Swift/Swift-Algorithm/BOJ/6. 문자열/1157.swift
+++ Swift/Swift-Algorithm/BOJ/6. 문자열/1157.swift
@@ -9,7 +9,7 @@
 
 class BOJ1157: Solvable {
     func run() {
-        solution1()
+        solution2()
     }
 
     // 메모리: 85708KB, 시간: 156ms, 코드 길이: 528B
@@ -47,8 +47,8 @@ class BOJ1157: Solvable {
         }
     }
 
-    // 메모리: 81520KB, 시간: 108ms, 코드 길이: 486B
-    // 출처: https://www.acmicpc.net/source/43965449
+    // 메모리: 71112B, 시간: 108ms, 코드 길이: 466B
+    // 원문 출처: https://www.acmicpc.net/source/43965449
     private func solution2() {
         let word = readLine()!.uppercased()
         var table: Dictionary<Character, Int> = [:]
@@ -58,7 +58,7 @@ class BOJ1157: Solvable {
 
         if table.count == 1 {
             print(word[word.startIndex])
-        } else { // count > 1
+        } else {
             let values = table.values.sorted(by: >)
             if values[0] == values[1] {
                 print("?")
@@ -68,6 +68,73 @@ class BOJ1157: Solvable {
                         print(key)
                         break
                     }
+                }
+            }
+        }
+    }
+
+    // 메모리: 85712KB, 시간: 196ms, 코드 길이: 453B
+    private func solution3() {
+        let input = Array(readLine()!.uppercased())
+        let alphabet = Array("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        var counter = Array(repeating: 0, count: 26)
+
+        for char in alphabet.indices {
+            for word in input {
+                if alphabet[char] == word {
+                    counter[char] += 1
+                }
+            }
+        }
+
+        if counter.firstIndex(of: counter.max()!) != counter.lastIndex(of: counter.max()!) {
+            print("?")
+        } else {
+            print(alphabet[counter.firstIndex(of: counter.max()!)!])
+        }
+    }
+
+    // 메모리: 85712KB, 시간: 180ms, 코드 길이: 356B
+    private func solution4() {
+        let input = Array(readLine()!.uppercased())
+        var alphabet = [Character: Int]()
+        var maxChar = [Character]()
+
+        input.forEach {
+            if alphabet[$0] == nil {
+                alphabet[$0] = 0
+            }
+
+            alphabet[$0]! += 1
+        }
+
+        for char in alphabet.keys where alphabet[char] == alphabet.values.max() {
+            maxChar.append(char)
+        }
+
+        print(maxChar.count == 1 ? maxChar[0] : "?")
+    }
+
+    // 메모리: 71112KB, 시간: 144ms, 코드 길이: 423B
+    private func solution5() {
+        let input = readLine()!.uppercased()
+        var alphabet = [Character: Int]()
+
+        input.forEach {
+            alphabet[$0, default: 0] += 1
+        }
+
+        let values = alphabet.values.sorted(by: >)
+
+        if alphabet.count == 1 {
+            print(alphabet[alphabet.startIndex].key)
+        } else if values[0] == values[1] {
+            print("?")
+        } else {
+            for (key, value) in alphabet {
+                if value == values[0] {
+                    print(key)
+                    break
                 }
             }
         }
