4a804abc9fb613b830b728add454d1cbb13538de
taek0622 <min4644@gmail.com>
2023-01-04 02:46:52 +0900

    Feat:sparkles: Add BOJ751 solution2

--- Swift/Swift-Algorithm/BOJ/10. 정렬/2751.swift
+++ Swift/Swift-Algorithm/BOJ/10. 정렬/2751.swift
@@ -6,10 +6,14 @@
 //
 
 // 문제: https://www.acmicpc.net/problem/2751
-
+import Foundation
 class BOJ2751: Solvable {
     func run() {
-        // 메모리: 89152KB, 시간: 1364ms, 코드 길이: 149B
+        solution2()
+    }
+
+    // 메모리: 89152KB, 시간: 1364ms, 코드 길이: 149B
+    private func solution1() {
         let N = Int(readLine()!)!
         var numbers = [Int]()
 
@@ -20,5 +24,17 @@ class BOJ2751: Solvable {
         numbers.sorted().forEach {
             print($0)
         }
+    }
+
+    // 메모리: 106732KB, 시간: 780ms, 코드 길이: 173B
+    private func solution2() {
+        let N = Int(readLine()!)!
+        var numbers = [Int]()
+
+        for _ in 0..<N {
+            numbers.append(Int(readLine()!)!)
+        }
+
+        print(numbers.sorted().map { String($0) }.joined(separator: "\n"))
     }
 }
