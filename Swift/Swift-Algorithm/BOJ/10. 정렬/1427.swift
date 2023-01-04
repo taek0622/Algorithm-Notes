12bdd0593bf4466c88705c80724262888a6f64c8
taek0622 <min4644@gmail.com>
2023-01-04 23:35:35 +0900

    Feat:sparkles:: Add BOJ1427 solution2

--- Swift/Swift-Algorithm/BOJ/10. 정렬/1427.swift
+++ Swift/Swift-Algorithm/BOJ/10. 정렬/1427.swift
@@ -9,7 +9,11 @@
 
 class BOJ1427: Solvable {
     func run() {
-        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 132B
+        solution2()
+    }
+
+    // 메모리: 69104KB, 시간: 8ms, 코드 길이: 132B
+    private func solution1() {
         let N = readLine()!.map { Int(String($0))! }
         var result = ""
 
@@ -18,5 +22,10 @@ class BOJ1427: Solvable {
         }
 
         print(result)
+    }
+
+    // 메모리: 69104KB, 시간: 8ms, 코드 길이: 60B
+    private func solution2() {
+        print(readLine()!.map { String($0) }.sorted(by: >).joined())
     }
 }
