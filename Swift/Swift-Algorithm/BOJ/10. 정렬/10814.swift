eb209d424c6cfb57611594f558fcfd25827782bb
taek0622 <min4644@gmail.com>
2023-01-05 20:10:30 +0900

    Feat:sparkles:: Add BOJ10814 new solution

--- Swift/Swift-Algorithm/BOJ/10. 정렬/10814.swift
+++ Swift/Swift-Algorithm/BOJ/10. 정렬/10814.swift
@@ -9,7 +9,11 @@
 
 class BOJ10814: Solvable {
     func run() {
-        // 메모리: 73820KB, 시간: 152ms, 코드 길이: 482B
+        solution3()
+    }
+
+    // 메모리: 73820KB, 시간: 152ms, 코드 길이: 482B
+    private func solution1() {
         let N = Int(readLine()!)!
         var members = [(Int, String)]()
         var result = ""
@@ -28,6 +32,41 @@ class BOJ10814: Solvable {
         }
 
         members.sort(by: sortingMembers)
+
+        members.forEach {
+            result += "\($0.0) \($0.1)\n"
+        }
+
+        print(result)
+    }
+
+    // 메모리: 75180KB, 시간: 152ms, 코드 길이: 294B
+    private func solution2() {
+        let N = Int(readLine()!)!
+        var members = [(Int, String)]()
+
+        for _ in 0..<N {
+            let input = readLine()!.split(separator: " ").map { String($0) }
+            members.append((Int(input[0])!, input[1]))
+        }
+
+        members.sort(by: { $0.0 < $1.0 })
+
+        print(members.map { "\($0.0) \($0.1)" }.joined(separator: "\n"))
+    }
+
+    // 메모리: 73820KB, 시간: 152ms, 코드 길이: 314B
+    private func solution3() {
+        let N = Int(readLine()!)!
+        var members = [(Int, String)]()
+        var result = ""
+
+        for _ in 0..<N {
+            let input = readLine()!.split(separator: " ").map { String($0) }
+            members.append((Int(input[0])!, input[1]))
+        }
+
+        members.sort(by: { $0.0 < $1.0 })
 
         members.forEach {
             result += "\($0.0) \($0.1)\n"
