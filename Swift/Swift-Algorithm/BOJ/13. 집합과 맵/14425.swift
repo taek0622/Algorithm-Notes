824e15e3f61c14d6042dc500da74caa5b9d9c69c
taek0622 <min4644@gmail.com>
2023-01-08 19:43:16 +0900

    Feat:sparkles:: Add BOJ14425

---
+++ Swift/Swift-Algorithm/BOJ/13. 집합과 맵/14425.swift
@@ -0,0 +1,29 @@
+//
+//  14425.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/01/08.
+//
+
+// 문제: https://www.acmicpc.net/problem/14425
+
+class BOJ14425: Solvable {
+    func run() {
+        // 메모리: 74956KB, 시간: 148ms, 코드 길이: 258B
+        let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var S = Set<String>()
+        var count = 0
+
+        for _ in 0..<input[0] {
+            S.insert(readLine()!)
+        }
+
+        for _ in 0..<input[1] {
+            if S.contains(readLine()!) {
+                count += 1
+            }
+        }
+
+        print(count)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ10815()
+let main = BOJ14425()
 main.run()
