92461944a70835d0ed5b066dc42643f1f464e943
taek0622 <min4644@gmail.com>
2023-01-12 21:22:12 +0900

    Chore:wrench:: Modify BOJ3036 solution

--- Swift/Swift-Algorithm/BOJ/15. 배수와 약수/3036.swift
+++ Swift/Swift-Algorithm/BOJ/15. 배수와 약수/3036.swift
@@ -9,17 +9,13 @@
 
 class BOJ3036: Solvable {
     func run() {
-        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 453B
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 348B
         let N = Int(readLine()!)!
         let radii = readLine()!.split(separator: " ").map { Int(String($0))! }
 
         for idx in 1..<N {
-            if radii[0] % radii[idx] == 0 {
-                print("\(radii[0]/radii[idx])/1")
-            } else {
-                let divisor = findDivisor(radii[0], radii[idx])
-                print("\(radii[0]/divisor)/\(radii[idx]/divisor)")
-            }
+            let divisor = findDivisor(radii[0], radii[idx])
+            print("\(radii[0]/divisor)/\(radii[idx]/divisor)")
         }
 
         func findDivisor(_ num1: Int, _ num2: Int) -> Int {
