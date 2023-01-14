3b94538aa46c63610bc06852b43dbca0a0408b65
taek0622 <min4644@gmail.com>
2023-01-15 00:23:48 +0900

    Feat:sparkles:: Add BOJ2447

---
+++ Swift/Swift-Algorithm/BOJ/11. 재귀/2447.swift
@@ -0,0 +1,77 @@
+//
+//  2447.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/01/06.
+//
+
+// 문제: https://www.acmicpc.net/problem/2447
+
+class BOJ2447: Solvable {
+    func run() {
+        solution2()
+    }
+
+    // 메모리: 76980KB, 시간: 836ms, 코드 길이: 501B
+    private func solution1() {
+        let N = Int(readLine()!)!
+        var result = ""
+
+        for row in 0..<N {
+            for column in 0..<N {
+                result += printStar(N, row, column)
+            }
+
+            result += "\n"
+        }
+
+        print(result)
+
+        func printStar(_ N: Int, _ row: Int, _ column: Int) -> String {
+            var stars = ""
+
+            if ((row / N) % 3 == 1) && ((column / N) % 3 == 1) {
+                stars += " "
+            } else {
+                if N / 3 == 0 {
+                    stars += "*"
+                } else {
+                    stars += printStar(N/3, row, column)
+                }
+            }
+
+            return stars
+        }
+    }
+
+    // 메모리: 88612KB, 시간: 52ms, 코드 길이: 522B
+    private func solution2() {
+        let N = Int(readLine()!)!
+
+        print(printStar(N).joined(separator: "\n"))
+
+        func printStar(_ N: Int) -> [String] {
+            if N == 1 {
+                return ["*"]
+            }
+
+            let pattern = printStar(N/3)
+
+            var result = [String]()
+
+            for star in pattern {
+                result.append(String(repeating: star, count: 3))
+            }
+
+            for star in pattern {
+                result.append(star + String(repeating: " ", count: N/3) + star)
+            }
+
+            for star in pattern {
+                result.append(String(repeating: star, count: 3))
+            }
+
+            return result
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ24060()
+let main = BOJ2447()
 main.run()
