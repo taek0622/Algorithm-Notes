26b4716c15c0b19a8f05f3ded6d9243cc5a16263
taek0622 <min4644@gmail.com>
2023-01-17 03:51:15 +0900

    Feat:sparkles:: Add BOJ3053 solution2

--- Swift/Swift-Algorithm/BOJ/14. 기하 1/3053.swift
+++ Swift/Swift-Algorithm/BOJ/14. 기하 1/3053.swift
@@ -11,12 +11,23 @@ import Foundation
 
 class BOJ3053: Solvable {
     func run() {
-        // 메모리: 79504KB, 시간: 12ms, 코드 길이: 315B
+        solution2()
+    }
+
+    // 메모리: 79504KB, 시간: 12ms, 코드 길이: 315B
+    private func solution1() {
         let radius = Double(readLine()!)!
         let euclid = String(format: "%.6f", (radius * radius * Double.pi * 1000000).rounded() / 1000000)
         let taxi_radius = sqrt(radius * radius * 2)
         let taxi = String(format: "%.6f", (taxi_radius * taxi_radius * 1000000).rounded() / 1000000)
 
         print("\(euclid)\n\(taxi)")
+    }
+
+    // 메모리: 79504KB, 시간: 12ms, 코드 길이: 165B
+    private func solution2() {
+        let radius = Double(readLine()!)!
+
+        print("\(String(format: "%.6f", radius * radius * Double.pi))\n\(String(format: "%.6f", radius * radius * 2))")
     }
 }
