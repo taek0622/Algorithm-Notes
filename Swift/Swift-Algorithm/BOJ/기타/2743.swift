6077a268e81b3d0bd75a3596a8d79f31ef4e3cc5
taek0622 <min4644@gmail.com>
2023-01-24 21:49:27 +0900

    Feat:sparkles:: Add BOJ2743

---
+++ Swift/Swift-Algorithm/BOJ/기타/2743.swift
@@ -0,0 +1,17 @@
+//
+//  2743.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/01/24.
+//
+
+// 문제: https://www.acmicpc.net/problem/2743
+
+class BOJ2743: Solvable {
+    func run() {
+        // 메모리: 69096KB, 시간: 8ms, 코드 길이: 49B
+        let word = String(readLine()!)
+
+        print(word.count)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ2420()
+let main = BOJ2743()
 main.run()
