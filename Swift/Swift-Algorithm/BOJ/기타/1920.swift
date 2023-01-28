fb37dfbeaae693a582ee581c97a96deb8185c01b
taek0622 <min4644@gmail.com>
2023-01-28 21:51:18 +0900

    Chore:wrench:: Modify BOJ1920 Logic

--- Swift/Swift-Algorithm/BOJ/기타/1920.swift
+++ Swift/Swift-Algorithm/BOJ/기타/1920.swift
@@ -9,14 +9,17 @@
 
 class BOJ1920: Solvable {
     func run() {
-        // 메모리: 81396KB, 시간: 208ms, 코드 길이: 247B
+        // 메모리: 81396KB, 시간: 148ms, 코드 길이: 289B
         let N = Int(readLine()!)!
         let A = Set(readLine()!.split(separator: " ").map { Int(String($0))! })
         let M = Int(readLine()!)!
         let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var result = ""
 
         input.forEach {
-            print(A.contains($0) ? 1 : 0)
+            result += A.contains($0) ? "1\n" : "0\n"
         }
+
+        print(result)
     }
 }
