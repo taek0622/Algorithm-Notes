923173b798bf5dd38fc2af9dfe4ca3e8b8508a2d
taek0622 <min4644@gmail.com>
2023-01-30 03:33:47 +0900

    Feat:sparkles:: Add BOJ2558

---
+++ Swift/Swift-Algorithm/BOJ/기타/2558.swift
@@ -0,0 +1,18 @@
+//
+//  2558.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/01/30.
+//
+
+// 문제: https://www.acmicpc.net/problem/2558
+
+class BOJ2558: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 65B
+        let A = Int(readLine()!)!
+        let B = Int(readLine()!)!
+
+        print(A + B)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ1920()
+let main = BOJ2558()
 main.run()
