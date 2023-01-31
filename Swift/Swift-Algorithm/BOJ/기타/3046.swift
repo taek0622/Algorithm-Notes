83937a13b37cb8835a39cbef338a10d28c9301ec
taek0622 <min4644@gmail.com>
2023-01-31 23:05:21 +0900

    Feat:sparkles:: Add BOJ3046

---
+++ Swift/Swift-Algorithm/BOJ/기타/3046.swift
@@ -0,0 +1,17 @@
+//
+//  3046.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/01/31.
+//
+
+// 문제: https://www.acmicpc.net/problem/3046
+
+class BOJ3046: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 102B
+        let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+        print(input[1] * 2 - input[0])
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ2747()
+let main = BOJ3046()
 main.run()
