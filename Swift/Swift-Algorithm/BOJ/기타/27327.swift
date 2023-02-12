6f435ec55db0099878fbe172f2bc54cb8f35e1e2
taek0622 <min4644@gmail.com>
2023-02-13 03:11:04 +0900

    BOJ27327: 時間 (Hour)
    - 수학, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/기타/27327.swift
@@ -0,0 +1,15 @@
+//
+//  27327.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/02/13.
+//
+
+// 문제: https://www.acmicpc.net/problem/27327
+
+class BOJ27327: Solvable {
+    func run() {
+        // 메모리: 69096KB, 시간: 8ms, 코드 길이: 29B
+        print(Int(readLine()!)! * 24)
+    }
+}
