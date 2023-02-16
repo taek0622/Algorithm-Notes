3cd13dd9419f828717a7d2c9fdb1343f91887fc7
taek0622 <min4644@gmail.com>
2023-02-16 10:06:56 +0900

    BOJ2845: 파티가 끝나고 난 뒤 (solution2)
    - 수학, 구현, 사칙연산

--- Swift/Swift-Algorithm/BOJ/분류별/수학/2845.swift
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/2845.swift
@@ -9,12 +9,23 @@
 
 class BOJ2845: Solvable {
     func run() {
-        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 228B
+        solution2()
+    }
+
+    // 메모리: 69104KB, 시간: 8ms, 코드 길이: 228B
+    private func solution1() {
         let input = readLine()!.split(separator: " ").map { Int(String($0))! }
         let personnels = readLine()!.split(separator: " ").map { Int(String($0))! }
 
         for personnel in personnels {
             print("\(personnel - input[0] * input[1])", terminator: " ")
         }
+    }
+
+    // 메모리: 69104KB, 시간: 8ms, 코드 길이: 197B
+    private func solution2() {
+        let personnel = readLine()!.split(separator: " ").map { Int(String($0))! }.reduce(1, *)
+
+        print(readLine()!.split(separator: " ").map { String(Int(String($0))! - personnel) }.joined(separator: " "))
     }
 }
