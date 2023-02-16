60a0bda4e6600d846b262f604a08a00fd725d372
taek0622 <min4644@gmail.com>
2023-02-16 12:20:45 +0900

    BOJ1463: 1로 만들기
    - 다이나믹 프로그래밍

---
+++ Swift/Swift-Algorithm/BOJ/분류별/다이나믹 프로그래밍/1463.swift
@@ -0,0 +1,40 @@
+//
+//  1463.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/02/16.
+//
+
+// 문제: https://www.acmicpc.net/problem/1463
+
+class BOJ1463: Solvable {
+    func run() {
+        solution1()
+    }
+
+    // 메모리: 76916KB, 시간: 12ms, 코드 길이: 304B
+    private func solution1() {
+        var numbers = Array(repeating: 0, count: Int(readLine()!)! + 1)
+
+        print(becomeOne(numbers.count - 1))
+
+        func becomeOne(_ X: Int) -> Int {
+            if X < 2 || numbers[X] != 0 {
+                return numbers[X]
+            }
+
+            numbers[X] = min(becomeOne(X / 3) + X % 3, becomeOne(X / 2) + X % 2) + 1
+
+            return numbers[X]
+        }
+    }
+
+    // 메모리: 69100KB, 시간: 8ms, 코드 길이: 155B
+    private func solution2() {
+        print(becomeOne(Int(readLine()!)!))
+
+        func becomeOne(_ X: Int) -> Int {
+            return X < 2 ? 0 : min(becomeOne(X / 2) + X % 2, becomeOne(X / 3) + X % 3) + 1
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ2501()
+let main = BOJ1463()
 main.run()
