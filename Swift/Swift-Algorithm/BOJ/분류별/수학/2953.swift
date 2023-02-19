2536a45f896b71c65f14b02dc835d58b1463b1df
taek0622 <min4644@gmail.com>
2023-02-19 22:10:22 +0900

    BOJ2953: 나는 요리사다
    - 수학, 구현, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/2953.swift
@@ -0,0 +1,25 @@
+//
+//  2953.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/02/19.
+//
+
+// 문제: https://www.acmicpc.net/problem/2953
+
+class BOJ2953: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 226B
+        var winner = (0, 0)
+
+        for idx in 1...5 {
+            let score = readLine()!.split(separator: " ").map { Int(String($0))! }.reduce(0, +)
+
+            if score > winner.1 {
+                winner = (idx, score)
+            }
+        }
+
+        print("\(winner.0) \(winner.1)")
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ5522()
+let main = BOJ2953()
 main.run()
