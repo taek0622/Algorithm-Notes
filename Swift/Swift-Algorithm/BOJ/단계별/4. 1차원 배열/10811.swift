d6f84b908e8d20d2dfe5ebb1d159c29bab545646
taek0622 <min4644@gmail.com>
2023-02-28 11:27:47 +0900

    BOJ10811: 바구니 뒤집기 (solution2)
    - 구현, 시뮬레이션

--- Swift/Swift-Algorithm/BOJ/단계별/4. 1차원 배열/10811.swift
+++ Swift/Swift-Algorithm/BOJ/단계별/4. 1차원 배열/10811.swift
@@ -9,13 +9,30 @@
 
 class BOJ10811: Solvable {
     func run() {
-        // 메모리: 69108KB, 시간: 8ms, 코드 길이: 289B
+        solution2()
+    }
+
+    // 메모리: 69108KB, 시간: 8ms, 코드 길이: 289B
+    private func solution1() {
         let NM = readLine()!.split(separator: " ").map { Int(String($0))! }
         var basket = Array(1...NM[0])
 
         for _ in 0..<NM[1] {
             let input = readLine()!.split(separator: " ").map { Int(String($0))! - 1 }
             basket[input[0]...input[1]].reverse()
+        }
+
+        basket.forEach { print($0, terminator: " ") }
+    }
+
+    // 메모리: 69104KB, 시간: 8ms, 코드 길이: 341B
+    private func solution2() {
+        let NM = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var basket = Array(1...NM[0])
+
+        for _ in 0..<NM[1] {
+            let input = readLine()!.split(separator: " ").map { Int(String($0))! - 1 }
+            basket.replaceSubrange(input[0]...input[1], with: basket[input[0]...input[1]].reversed())
         }
 
         basket.forEach { print($0, terminator: " ") }
