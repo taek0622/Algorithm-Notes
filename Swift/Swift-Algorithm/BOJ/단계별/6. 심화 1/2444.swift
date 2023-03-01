e1c014c14ea8955d8abe7adf2eed6d4b24ce732b
taek0622 <min4644@gmail.com>
2023-03-01 23:32:02 +0900

    BOJ2444: 별 찍기 - 7
    - 구현

---
+++ Swift/Swift-Algorithm/BOJ/단계별/6. 심화 1/2444.swift
@@ -0,0 +1,45 @@
+//
+//  2444.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/03/01.
+//
+
+// 문제: https://www.acmicpc.net/problem/2444
+
+class BOJ2444: Solvable {
+    func run() {
+        solution2()
+    }
+
+    // 메모리: 69100KB, 시간: 12ms, 코드 길이: 301B
+    private func solution1() {
+        let N = Int(readLine()!)!
+        var stars = [String]()
+
+        for number in 1...N {
+            stars.append(String(repeating: " ", count: N-number) + String(repeating: "*", count: number * 2 - 1))
+        }
+
+        for idx in 0..<2*N-1 {
+            if idx < N {
+                print(stars[idx])
+            } else {
+                print(stars[2*(N-1)-idx])
+            }
+        }
+    }
+
+    // 메모리: 69100KB, 시간: 12ms, 코드 길이: 294B
+    private func solution2() {
+        let N = Int(readLine()!)!
+
+        for number in (0..<N).reversed() {
+            print(String(repeating: " ", count: number) + String(repeating: "*", count: (N - number) * 2 - 1))
+        }
+
+        for number in 1..<N {
+            print(String(repeating: " ", count: number) + String(repeating: "*", count: (N - number) * 2 - 1))
+        }
+    }
+}
