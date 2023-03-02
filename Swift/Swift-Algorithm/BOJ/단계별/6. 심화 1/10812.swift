5bc9a9d280fa43e65c45f653763c49c6e1e57f68
taek0622 <min4644@gmail.com>
2023-03-02 23:25:49 +0900

    BOJ10812: 바구니 순서 바꾸기 (solution2)
    - 구현, 시뮬레이션

--- Swift/Swift-Algorithm/BOJ/단계별/6. 심화 1/10812.swift
+++ Swift/Swift-Algorithm/BOJ/단계별/6. 심화 1/10812.swift
@@ -9,7 +9,11 @@
 
 class BOJ10812: Solvable {
     func run() {
-        // 메모리: 69108KB, 시간: 8ms, 코드 길이: 391B
+        solution1()
+    }
+
+    // 메모리: 69108KB, 시간: 8ms, 코드 길이: 391B
+    private func solution1() {
         let NM = readLine()!.split(separator: " ").map { Int(String($0))! }
         var basket = Array(1...NM[0])
 
@@ -18,6 +22,19 @@ class BOJ10812: Solvable {
             let part = basket[input[0]..<input[2]]
             basket.insert(contentsOf: part, at: input[1] + 1)
             basket.removeSubrange(input[0]..<input[2])
+        }
+
+        basket.forEach { print($0, terminator: " ") }
+    }
+
+    // 메모리: 69112KB, 시간: 8ms, 코드 길이: 339B
+    private func solution2() {
+        let NM = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var basket = Array(1...NM[0])
+
+        for _ in 0..<NM[1] {
+            let input = readLine()!.split(separator: " ").map { Int(String($0))! - 1 }
+            basket[input[0]...input[1]] = basket[input[2]...input[1]] + basket[input[0]..<input[2]]
         }
 
         basket.forEach { print($0, terminator: " ") }
