f32d151f2460114132471b7480f98c7a0d8c8840
taek0622 <min4644@gmail.com>
2023-03-02 23:21:18 +0900

    BOJ10812: 바구니 순서 바꾸기
    - 구현, 시뮬레이션

---
+++ Swift/Swift-Algorithm/BOJ/단계별/6. 심화 1/10812.swift
@@ -0,0 +1,25 @@
+//
+//  10812.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/03/02.
+//
+
+// 문제: https://www.acmicpc.net/problem/10812
+
+class BOJ10812: Solvable {
+    func run() {
+        // 메모리: 69108KB, 시간: 8ms, 코드 길이: 391B
+        let NM = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var basket = Array(1...NM[0])
+
+        for _ in 0..<NM[1] {
+            let input = readLine()!.split(separator: " ").map { Int(String($0))! - 1 }
+            let part = basket[input[0]..<input[2]]
+            basket.insert(contentsOf: part, at: input[1] + 1)
+            basket.removeSubrange(input[0]..<input[2])
+        }
+
+        basket.forEach { print($0, terminator: " ") }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ2444()
+let main = BOJ10812()
 main.run()
