a9fbb7e9ecde9b3d1b19f0a099a2e3e9b51baa7e
taek0622 <min4644@gmail.com>
2023-03-04 12:17:43 +0900

    BOJ25206: 너의 평점은 (new solution)
    - 수학, 구현, 문자열

--- Swift/Swift-Algorithm/BOJ/단계별/6. 심화 1/25206.swift
+++ Swift/Swift-Algorithm/BOJ/단계별/6. 심화 1/25206.swift
@@ -11,7 +11,7 @@ import Foundation
 
 class BOJ25206: Solvable {
     func run() {
-        solution3()
+        solution4()
     }
 
     // 메모리: 79512KB, 시간: 12ms, 코드 길이: 481B
@@ -76,5 +76,88 @@ class BOJ25206: Solvable {
                 return 0.0
             }
         }
+    }
+
+    // 메모리: 79512KB, 시간: 12ms, 코드 길이: 922B
+    private func solution3() {
+        var count = 0.0
+        var score = 0.0
+
+        for _ in 0..<20 {
+            let input = readLine()!.split(separator: " ").map { String($0) }
+
+            if input[2] != "P" {
+                count += Double(input[1])!
+
+                switch input[2] {
+                case "A+":
+                    score += Double(input[1])! * 4.5
+                case "A0":
+                    score += Double(input[1])! * 4.0
+                case "B+":
+                    score += Double(input[1])! * 3.5
+                case "B0":
+                    score += Double(input[1])! * 3.0
+                case "C+":
+                    score += Double(input[1])! * 2.5
+                case "C0":
+                    score += Double(input[1])! * 2.0
+                case "D+":
+                    score += Double(input[1])! * 1.5
+                case "D0":
+                    score += Double(input[1])! * 1.0
+                case "F":
+                    fallthrough
+                default:
+                    break
+                }
+            }
+        }
+
+        print(round(score / count * 1000000) / 1000000)
+    }
+
+    // 메모리: 69104KB, 시간: 8ms, 코드 길이: 528B
+    private func solution4() {
+        let scores: [String: Float] = ["A+": 4.5, "A0": 4.0,
+                                       "B+": 3.5, "B0": 3.0,
+                                       "C+": 2.5, "C0": 2.0,
+                                       "D+": 1.5, "D0": 1.0,
+                                       "F": 0.0]
+        var count: Float = 0
+        var score: Float = 0
+
+        for _ in 0..<20 {
+            let input = readLine()!.split(separator: " ").map { String($0) }
+
+            if input[2] != "P" {
+                count += Float(input[1])!
+                score += (Float(input[1])! * scores[input[2]]!)
+            }
+        }
+
+        print(score / count)
+    }
+
+    // 메모리: 69104KB, 시간: 8ms, 코드 길이: 435B
+    private func solution5() {
+        let scores = ["A+": 4.5, "A0": 4.0,
+                                       "B+": 3.5, "B0": 3.0,
+                                       "C+": 2.5, "C0": 2.0,
+                                       "D+": 1.5, "D0": 1.0,
+                                       "F": 0.0]
+        var count = 0.0
+        var score = 0.0
+
+        for _ in 0..<20 {
+            let input = readLine()!.split(separator: " ").map { String($0) }
+
+            if input[2] != "P" {
+                count += Double(input[1])!
+                score += (Double(input[1])! * scores[input[2]]!)
+            }
+        }
+
+        print(score / count)
     }
 }
