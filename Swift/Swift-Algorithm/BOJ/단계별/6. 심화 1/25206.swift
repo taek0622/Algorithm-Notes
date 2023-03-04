d570819dabbb6ca08c9fbe4847c33088eb6b0ecf
taek0622 <min4644@gmail.com>
2023-03-04 12:08:02 +0900

    BOJ25206: 너의 평점은
    - 수학, 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/단계별/6. 심화 1/25206.swift
@@ -0,0 +1,80 @@
+//
+//  25206.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/03/04.
+//
+
+// 문제: https://www.acmicpc.net/problem/25206
+
+import Foundation
+
+class BOJ25206: Solvable {
+    func run() {
+        solution3()
+    }
+
+    // 메모리: 79512KB, 시간: 12ms, 코드 길이: 481B
+    private func solution1() {
+        let scores = ["A+": 4.5, "A0": 4.0,
+                      "B+": 3.5, "B0": 3.0,
+                      "C+": 2.5, "C0": 2.0,
+                      "D+": 1.5, "D0": 1.0,
+                      "F": 0.0]
+        var count = 0.0
+        var score = 0.0
+
+        for _ in 0..<20 {
+            let input = readLine()!.split(separator: " ").map { String($0) }
+
+            if input[2] != "P" {
+                count += Double(input[1])!
+                score += (Double(input[1])! * scores[input[2]]!)
+            }
+        }
+
+        print(round(score / count * 1000000) / 1000000)
+    }
+
+    // 메모리: 79508KB, 시간: 12ms, 코드 길이: 766B
+    private func solution2() {
+        var count = 0.0
+        var score = 0.0
+
+        for _ in 0..<20 {
+            let input = readLine()!.split(separator: " ").map { String($0) }
+
+            if input[2] != "P" {
+                count += Double(input[1])!
+                score += (Double(input[1])! * matchScore(input[2]))
+            }
+        }
+
+        print(round(score / count * 1000000) / 1000000)
+
+        func matchScore(_ score: String) -> Double {
+            switch score {
+            case "A+":
+                return 4.5
+            case "A0":
+                return 4.0
+            case "B+":
+                return 3.5
+            case "B0":
+                return 3.0
+            case "C+":
+                return 2.5
+            case "C0":
+                return 2.0
+            case "D+":
+                return 1.5
+            case "D0":
+                return 1.0
+            case "F":
+                return 0.0
+            default:
+                return 0.0
+            }
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ10988()
+let main = BOJ25206()
 main.run()
