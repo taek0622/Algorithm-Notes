d0c5bcd8c7e35748f9a7cfd3c8c4036adccdda9f
taek0622 <min4644@gmail.com>
2023-03-13 00:11:36 +0900

    BOJ2960: 에라토스테네스의 체 (solution2)
    - 수학, 구현, 정수론, 소수 판정, 에라토스테네스의 체

--- Swift/Swift-Algorithm/BOJ/분류별/수학/2960.swift
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/2960.swift
@@ -9,22 +9,42 @@
 
 class BOJ2960: Solvable {
     func run() {
-        // 메모리: 69108KB, 시간: 8ms, 코드 길이: 438B
-        let input = readLine()!.split(separator: " ").map { Int(String($0))! }
-        var numbers = Set(2...input[0])
-        var divisor = 2
-        var count = 0
+        solution2()
+    }
 
-        outer: while true {
+    // 메모리: 69108KB, 시간: 8ms, 코드 길이: 394B
+    private func solution1() {
+        var input = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var numbers = Set(2...input[0])
+
+        outer: for divisor in 2...input[0] {
             for number in stride(from: divisor, through: input[0], by: divisor) where numbers.contains(number) {
-                let result = numbers.remove(number)
-                count += 1
-                if count == input[1] {
-                    print(result!)
+                numbers.remove(number)
+                input[1] -= 1
+
+                if input[1] == 0 {
+                    print(number)
                     break outer
                 }
             }
-            divisor += 1
+        }
+    }
+
+    // 메모리: 69104KB, 시간: 8ms, 코드 길이: 400B
+    private func solution2() {
+        var input = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var numbers = Array(repeating: true, count: input[0] + 1)
+
+        outer: for divisor in 2...input[0] {
+            for idx in stride(from: divisor, through: input[0], by: divisor) where numbers[idx] {
+                numbers[idx] = false
+                input[1] -= 1
+
+                if input[1] == 0 {
+                    print(idx)
+                    break outer
+                }
+            }
         }
     }
 }
