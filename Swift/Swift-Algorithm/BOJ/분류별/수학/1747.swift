9e2b41813d77b4b1b20e1f3bced8587a0ab96592
taek0622 <min4644@gmail.com>
2023-03-18 14:57:00 +0900

    BOJ1747: 소수&팰린드롬
    - 수학, 브루트포스 알고리즘, 정수론, 소수 판정, 에라토스테네스의 체

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/1747.swift
@@ -0,0 +1,30 @@
+//
+//  1747.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/03/18.
+//
+
+// 문제: https://www.acmicpc.net/problem/1747
+
+class BOJ1747: Solvable {
+    func run() {
+        // 메모리: 70084KB, 시간: 84ms, 코드 길이: 429B
+        let N = Int(readLine()!)!
+        var numbers = Array(repeating: true, count: 1003002)
+        numbers[0...1] = [false, false]
+
+        for number in 2..<numbers.count where numbers[number] {
+            for idx in stride(from: number * number, to: numbers.count, by: number) {
+                numbers[idx] = false
+            }
+        }
+
+        for idx in N..<numbers.count where numbers[idx] {
+            if String(idx) == String(String(idx).reversed()) {
+                print(idx)
+                break
+            }
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ2749()
+let main = BOJ1747()
 main.run()
