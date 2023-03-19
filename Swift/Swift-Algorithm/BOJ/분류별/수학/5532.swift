d269370f0c3180dda647531666fd2d1d053e2b2c
taek0622 <min4644@gmail.com>
2023-03-19 14:14:37 +0900

    BOJ5532: 방학 숙제
    - 수학, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/5532.swift
@@ -0,0 +1,21 @@
+//
+//  5532.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/03/19.
+//
+
+// 문제: https://www.acmicpc.net/problem/5532
+
+class BOJ5532: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 169B
+        let L = Int(readLine()!)!
+        let A = Int(readLine()!)!
+        let B = Int(readLine()!)!
+        let C = Int(readLine()!)!
+        let D = Int(readLine()!)!
+
+        print(L - (max((A-1)/C, (B-1)/D) + 1))
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ1747()
+let main = BOJ5532()
 main.run()
