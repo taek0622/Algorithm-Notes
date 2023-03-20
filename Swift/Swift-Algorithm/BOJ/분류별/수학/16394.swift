4ba9cbae6d11a3969750f90b86295f190c80959d
taek0622 <min4644@gmail.com>
2023-03-20 11:11:27 +0900

    BOJ16394: 홍익대학교
    - 수학, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/16394.swift
@@ -0,0 +1,15 @@
+//
+//  16394.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/03/20.
+//
+
+// 문제: https://www.acmicpc.net/problem/16394
+
+class BOJ16394: Solvable {
+    func run() {
+        // 메모리: 69096KB, 시간: 8ms, 코드 길이: 31B
+        print(Int(readLine()!)! - 1946)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ5532()
+let main = BOJ16394()
 main.run()
