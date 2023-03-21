2065510cbc0544b27adb628dbcd774c87e6147f7
taek0622 <min4644@gmail.com>
2023-03-21 20:50:57 +0900

    BOJ5596: 시험 점수
    - 수학, 구현, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/5596.swift
@@ -0,0 +1,18 @@
+//
+//  5596.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/03/21.
+//
+
+// 문제: https://www.acmicpc.net/problem/5596
+
+class BOJ5596: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 177B
+        let S = readLine()!.split(separator: " ").map { Int(String($0))! }.reduce(0, +)
+        let T = readLine()!.split(separator: " ").map { Int(String($0))! }.reduce(0, +)
+
+        print(max(S, T))
+    }
+}
