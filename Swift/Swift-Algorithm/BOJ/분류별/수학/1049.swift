6670726d634c3467290b755a55d81e0576246e75
taek0622 <min4644@gmail.com>
2023-03-22 12:17:41 +0900

    BOJ1049: 기타줄
    - 수학, 그리디 알고리즘

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/1049.swift
@@ -0,0 +1,30 @@
+//
+//  1049.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/03/22.
+//
+
+// 문제: https://www.acmicpc.net/problem/1049
+
+class BOJ1049: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 490B
+        let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var setPrices = 1000
+        var singlePrices = 1000
+
+        for _ in 0..<input[1] {
+            let prices = readLine()!.split(separator: " ").map { Int(String($0))! }
+            if setPrices > prices[0] {
+                setPrices = prices[0]
+            }
+
+            if singlePrices > prices[1] {
+                singlePrices = prices[1]
+            }
+        }
+
+        print(min(((input[0] - 1) / 6 + 1) * setPrices, input[0] / 6 * setPrices + input[0] % 6 * singlePrices, singlePrices * input[0]))
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ5063()
+let main = BOJ1049()
 main.run()
