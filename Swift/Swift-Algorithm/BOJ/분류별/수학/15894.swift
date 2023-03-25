749bd953e9a507347d8e959a6422b4ad9d65a6f7
taek0622 <min4644@gmail.com>
2023-03-25 18:33:29 +0900

    BOJ15894: 수학은 체육과목 입니다
    - 수학, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/15894.swift
@@ -0,0 +1,15 @@
+//
+//  15894.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/03/25.
+//
+
+// 문제: https://www.acmicpc.net/problem/15894
+
+class BOJ15894: Solvable {
+    func run() {
+        // 메모리: 69096KB, 시간: 8ms, 코드 길이: 28B
+        print(Int(readLine()!)! * 4)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ2476()
+let main = BOJ15894()
 main.run()
