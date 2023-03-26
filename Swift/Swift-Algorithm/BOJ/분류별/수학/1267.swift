5b7477be021907f61023c4c5e6d20010985aca3e
taek0622 <min4644@gmail.com>
2023-03-26 16:59:41 +0900

    BOJ1267: 핸드폰 요금
    - 수학, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/1267.swift
@@ -0,0 +1,34 @@
+//
+//  1267.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/03/26.
+//
+
+// 문제: https://www.acmicpc.net/problem/1267
+
+class BOJ1267: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 325B
+        let N = Int(readLine()!)!
+        let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var (Y, M) = (0, 0)
+
+        for num in input {
+            Y += ((num / 30 + 1) * 10)
+            M += ((num / 60 + 1) * 15)
+        }
+
+        if Y <= M {
+            print("Y ", terminator: "")
+        }
+
+        if M <= Y {
+            print("M \(M)", terminator: "")
+        }
+
+        if Y < M {
+            print(Y)
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ1735()
+let main = BOJ1267()
 main.run()
