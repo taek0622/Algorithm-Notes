fa9c52a29013084c76d632920b812264f9605138
taek0622 <min4644@gmail.com>
2023-03-28 23:11:47 +0900

    BOJ27866: 문자와 문자열
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/단계별/6. 문자열/27866.swift
@@ -0,0 +1,25 @@
+//
+//  27866.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/03/28.
+//
+// 문제: https://www.acmicpc.net/problem/27866
+
+class BOJ27866: Solvable {
+    func run() {
+        solution2()
+    }
+
+    // 메모리: 69100KB, 시간: 8ms, 코드 길이: 66B
+    private func solution1() {
+        let S = Array(readLine()!)
+        let i = Int(readLine()!)!
+        print(S[i-1])
+    }
+
+    // 메모리: 69100KB, 시간: 8ms, 코드 길이: 46B
+    private func solution2() {
+        print(Array(readLine()!)[Int(readLine()!)!-1])
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ2745()
+let main = BOJ27866()
 main.run()
