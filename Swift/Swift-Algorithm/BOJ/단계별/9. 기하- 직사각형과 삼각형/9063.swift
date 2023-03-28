b2cfd9084acaf2fdc9a7bd417164497e7e84fb43
taek0622 <min4644@gmail.com>
2023-03-28 23:38:56 +0900

    BOJ9063: 대지
    - 수학, 구현, 기하학

---
+++ Swift/Swift-Algorithm/BOJ/단계별/9. 기하- 직사각형과 삼각형/9063.swift
@@ -0,0 +1,27 @@
+//
+//  9063.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/03/28.
+//
+
+// 문제: https://www.acmicpc.net/problem/9063
+
+class BOJ9063: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 76ms, 코드 길이: 411B
+        let N = Int(readLine()!)!
+        var minPoint = (10000, 10000)
+        var maxPoint = (-10000, -10000)
+
+        for _ in 0..<N {
+            let bead = readLine()!.split(separator: " ").map { Int(String($0))! }
+            minPoint.0 = min(minPoint.0, bead[0])
+            maxPoint.0 = max(maxPoint.0, bead[0])
+            minPoint.1 = min(minPoint.1, bead[1])
+            maxPoint.1 = max(maxPoint.1, bead[1])
+        }
+
+        print((maxPoint.1 - minPoint.1) * (maxPoint.0 - minPoint.0))
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ27323()
+let main = BOJ9063()
 main.run()
