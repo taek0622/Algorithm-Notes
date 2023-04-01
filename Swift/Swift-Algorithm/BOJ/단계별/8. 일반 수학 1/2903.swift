1cdc19d1977585292f19bd2c2917bdbafc87636a
taek0622 <min4644@gmail.com>
2023-04-01 20:01:04 +0900

    BOJ2903: 중앙 이동 알고리즘
    - 수학

---
+++ Swift/Swift-Algorithm/BOJ/단계별/8. 일반 수학 1/2903.swift
@@ -0,0 +1,42 @@
+//
+//  2903.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/04/01.
+//
+
+// 문제: https://www.acmicpc.net/problem/2903
+
+import Foundation
+
+class BOJ2903: Solvable {
+    func run() {
+        solution3()
+    }
+
+    // 메모리: 79508KB, 시간: 12ms, 코드 길이: 73B
+    private func solution1() {
+        let N = Int(readLine()!)!
+        print(pow(pow(2, N) + 1, 2))
+    }
+
+    // 메모리: 79508KB, 시간: 12ms, 코드 길이: 66B
+    private func solution2() {
+        let N = 1 << Int(readLine()!)! + 1
+        print(N * N)
+    }
+
+    // 메모리: 69100KB, 시간: 8ms, 코드 길이: 113B
+    private func solution3() {
+        let N = Int(readLine()!)!
+        var number = 1
+
+        for _ in 0..<N {
+            number *= 2
+        }
+
+        number += 1
+
+        print(number * number)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ2720()
+let main = BOJ2903()
 main.run()
