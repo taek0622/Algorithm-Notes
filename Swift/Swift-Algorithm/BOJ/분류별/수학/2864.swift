44c5aa89b275dfe67f074792f202191085374a90
taek0622 <min4644@gmail.com>
2023-04-03 01:49:47 +0900

    BOJ2864: 5와 6의 차이
    - 수학, 문자열, 그리디 알고리즘, 사칙연산

--- Swift/Swift-Algorithm/BOJ/분류별/수학/2864.swift
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/2864.swift
@@ -7,22 +7,32 @@
 
 // 문제: https://www.acmicpc.net/problem/2864
 
+import Foundation
+
 class BOJ2864: Solvable {
     func run() {
-        let input = readLine()!.split(separator: " ").map { Int(String($0) }
+        solution2()
+    }
+
+    // 메모리: 79512KB, 시간: 12ms, 코드 길이: 401B
+    private func solution1() {
+        let input = readLine()!.split(separator: " ").map { String($0) }
         var minSum = 0
         var maxSum = 0
 
-        if input[0] % 10 == 5 || input[0] % 10 == 6 {
-            minSum += (input[0] / 10) * 10 + 5
-            maxSum += (input[0] / 10) * 10 + 6
-        }
+        minSum += Int(input[0].replacingOccurrences(of: "6", with: "5"))!
+        minSum += Int(input[1].replacingOccurrences(of: "6", with: "5"))!
+        maxSum += Int(input[0].replacingOccurrences(of: "5", with: "6"))!
+        maxSum += Int(input[1].replacingOccurrences(of: "5", with: "6"))!
 
-        if input[1] % 10 == 5 || input[1] % 10 == 6 {
-            minSum += (input[1] / 10) * 10 + 5
-            maxSum += (input[1] / 10) * 10 + 6
-        }
+        print(minSum, maxSum)
+    }
 
+    // 메모리: 69108KB, 시간: 8ms, 코드 길이: 352B
+    private func solution2() {
+        let input = readLine()!.split(separator: " ").map { String($0) }
+        let minSum = Int(String(Array(input[0]).map { $0 == "6" ? "5" : $0 }))! + Int(String(Array(input[1]).map { $0 == "6" ? "5" : $0 }))!
+        let maxSum = Int(String(Array(input[0]).map { $0 == "5" ? "6" : $0 }))! + Int(String(Array(input[1]).map { $0 == "5" ? "6" : $0 }))!
         print(minSum, maxSum)
     }
 }
