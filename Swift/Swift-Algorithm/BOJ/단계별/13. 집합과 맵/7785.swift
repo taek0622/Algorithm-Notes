dafc1f5eed8f84d2a74d7a32b7e2f4978e02d12d
taek0622 <min4644@gmail.com>
2023-04-04 22:26:49 +0900

    BOJ7785: 회사에 있는 사람
    - 자료 구조, 해시를 사용한 집합과 맵

---
+++ Swift/Swift-Algorithm/BOJ/단계별/13. 집합과 맵/7785.swift
@@ -0,0 +1,30 @@
+//
+//  7785.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/04/04.
+//
+
+// 문제: https://www.acmicpc.net/problem/7785
+
+class BOJ7785: Solvable {
+    func run() {
+        // 메모리: 75600KB, 시간: 220ms, 코드 길이: 325B
+        let n = Int(readLine()!)!
+        var people = Set<String>()
+
+        for _ in 0..<n {
+            let input = readLine()!.split(separator: " ").map { String($0) }
+
+            if input[1] == "enter" {
+                people.insert(input[0])
+            } else if input[1] == "leave" {
+                people.remove(input[0])
+            }
+        }
+
+        people.sorted(by: >).forEach {
+            print($0)
+        }
+    }
+}
