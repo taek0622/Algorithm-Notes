b4642022dda115d04a6537e37962d2c1c8fe4821
taek0622 <min4644@gmail.com>
2023-04-10 20:14:15 +0900

    BOJ24262: 알고리즘 수업 - 알고리즘의 수행 시간 1
    - 구현, 시뮬레이션

---
+++ Swift/Swift-Algorithm/BOJ/단계별/11. 시간 복잡도/24262.swift
@@ -0,0 +1,14 @@
+//
+//  24262.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/04/10.
+//
+
+// 문제: https://www.acmicpc.net/problem/24262
+
+class BOJ24262: Solvable {
+    func run() {
+        print("1\n0")
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ5717()
+let main = BOJ24262()
 main.run()
