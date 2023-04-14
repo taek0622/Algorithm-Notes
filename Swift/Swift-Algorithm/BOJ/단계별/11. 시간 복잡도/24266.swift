ec8655fe6ff753fccbb09d932988336142dc8ee0
taek0622 <min4644@gmail.com>
2023-04-14 15:52:25 +0900

    BOJ24266: 알고리즘 수업 - 알고리즘의 수행 시간 5
    - 수학, 구현, 시뮬레이션

---
+++ Swift/Swift-Algorithm/BOJ/단계별/11. 시간 복잡도/24266.swift
@@ -0,0 +1,16 @@
+//
+//  24266.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/04/14.
+//
+
+// 문제: https://www.acmicpc.net/problem/24266
+
+class BOJ24266: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 46B
+        let n = Int(readLine()!)!
+        print("\(n*n*n)\n3")
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ24265()
+let main = BOJ24266()
 main.run()
