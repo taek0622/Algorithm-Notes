21541c8ca80c2706c248b8a667ac3aa920313a25
taek0622 <min4644@gmail.com>
2023-04-15 20:55:35 +0900

    BOJ24267: 알고리즘 수업 - 알고리즘의 수행 시간 6
    - 수학, 구현, 시뮬레이션

---
+++ Swift/Swift-Algorithm/BOJ/단계별/11. 시간 복잡도/24267.swift
@@ -0,0 +1,16 @@
+//
+//  24267.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/04/15.
+//
+
+// 문제: https://www.acmicpc.net/problem/24267
+
+class BOJ24267: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 56B
+        let n = Int(readLine()!)!
+        print("\(n*(n-1)*(n-2)/6)\n3")
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ24266()
+let main = BOJ24267()
 main.run()
