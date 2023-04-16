5ee8eb65f1b35fe5c29aa97b794fe0cf0d65a03b
taek0622 <min4644@gmail.com>
2023-04-16 19:54:41 +0900

    BOJ24313: 알고리즘 수업 - 점근적 표기 1
    - 수학

---
+++ Swift/Swift-Algorithm/BOJ/단계별/11. 시간 복잡도/24313.swift
@@ -0,0 +1,19 @@
+//
+//  24313.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/04/16.
+//
+
+// 문제: https://www.acmicpc.net/problem/24313
+
+class BOJ24313: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 183B
+        let a_n = readLine()!.split(separator: " ").map { Int(String($0))! }
+        let c = Int(readLine()!)!
+        let n0 = Int(readLine()!)!
+
+        print(a_n[0] * n0 + a_n[1] <= c * n0 && a_n[0] <= c ? 1 : 0)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ24267()
+let main = BOJ24313()
 main.run()
