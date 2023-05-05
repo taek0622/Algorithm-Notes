293137c4cfa577cdf3f908f7495eb9ed357ad0e7
taek0622 <min4644@gmail.com>
2023-05-05 14:28:14 +0900

    BOJ10984: 내 학점을 구해줘
    - 수학, 구현, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/10984.swift
@@ -0,0 +1,31 @@
+//
+//  10984.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/05/05.
+//
+
+// 문제: https://www.acmicpc.net/problem/10984
+
+import Foundation
+
+class BOJ10984: Solvable {
+    // 메모리: 79512KB, 시간: 12ms, 코드 길이: 357B
+    func run() {
+        let T = Int(readLine()!)!
+
+        for _ in 0..<T {
+            let N = Int(readLine()!)!
+            var credit = 0.0
+            var sum = 0.0
+
+            for _ in 0..<N {
+                let input = readLine()!.split(separator: " ").map { Double(String($0))! }
+                credit += input[0]
+                sum += input.reduce(1, *)
+            }
+
+            print(Int(credit), round(sum/credit * 10) / 10)
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ15969()
+let main = BOJ10984()
 main.run()
