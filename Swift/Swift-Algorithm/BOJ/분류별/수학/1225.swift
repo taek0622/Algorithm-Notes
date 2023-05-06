5a57f39aa19cebca4c4ef43c6dc23f39b8981eff
taek0622 <min4644@gmail.com>
2023-05-06 12:25:10 +0900

    BOJ1225: 이상한 곱셈
    - 수학, 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/1225.swift
@@ -0,0 +1,18 @@
+//
+//  1225.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/05/06.
+//
+
+// 문제: https://www.acmicpc.net/problem/1225
+
+class BOJ1225: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 12ms, 코드 길이: 199B
+        let input = readLine()!.split(separator: " ").map { String($0) }
+        let sum1 = input[0].map { Int(String($0))! }.reduce(0, +)
+        let sum2 = input[1].map { Int(String($0))! }.reduce(0, +)
+        print(sum1 * sum2)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ10984()
+let main = BOJ1225()
 main.run()
