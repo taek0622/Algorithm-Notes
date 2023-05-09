e5961ca4e46d6e9bf29260386f6ff0cc5980513d
taek0622 <min4644@gmail.com>
2023-05-09 09:23:35 +0900

    BOJ10569: 다면체
    - 수학, 기하학, 사칙연산, 오일러 지표 (X=v-e+f)

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/10569.swift
@@ -0,0 +1,20 @@
+//
+//  10569.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/05/09.
+//
+
+// 문제: https://www.acmicpc.net/problem/10569
+
+class BOJ10569: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 155B
+        let T = Int(readLine()!)!
+
+        for _ in 0..<T {
+            let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+            print(input[1] - input[0] + 2)
+        }
+    }
+}
