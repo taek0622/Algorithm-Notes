db14082f36b4193afa2ecfafba1b4166e1b8c8ba
taek0622 <min4644@gmail.com>
2023-05-11 14:19:03 +0900

    BOJ3058: 짝수를 찾아라
    - 수학, 구현, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/3058.swift
@@ -0,0 +1,46 @@
+//
+//  3058.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/05/11.
+//
+
+// 문제: https://www.acmicpc.net/problem/3058
+
+class BOJ3058: Solvable {
+    func run() {
+        solution2()
+    }
+
+    // 메모리: 69104KB, 시간: 12ms, 코드 길이: 299B
+    private func solution1() {
+        let T = Int(readLine()!)!
+
+        for _ in 0..<T {
+            let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+            var sum = 0
+            var min = 100
+
+            for num in input where num % 2 == 0 {
+                sum += num
+                
+                if num < min {
+                    min = num
+                }
+            }
+
+            print(sum, min)
+        }
+    }
+
+    // 메모리: 69104KB, 시간: 12ms, 코드 길이: 188B
+    private func solution2() {
+        let T = Int(readLine()!)!
+
+        for _ in 0..<T {
+            let input = readLine()!.split(separator: " ").map { Int(String($0))! }.filter { $0 % 2 == 0 }
+
+            print(input.reduce(0, +), input.min()!)
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ5988()
+let main = BOJ3058()
 main.run()
