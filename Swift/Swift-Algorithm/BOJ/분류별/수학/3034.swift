4474a1be226c98fbf9cda00ba750e5dae2fb0f60
taek0622 <min4644@gmail.com>
2023-05-14 10:41:59 +0900

    BOJ3034: 앵그리 창영
    - 수학, 기하학, 피타고라스 정리

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/3034.swift
@@ -0,0 +1,21 @@
+//
+//  3034.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/05/14.
+//
+
+// 문제: https://www.acmicpc.net/problem/3034
+
+class BOJ3034: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 219B
+        let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+        for _ in 0..<input[0] {
+            let length = Int(readLine()!)!
+
+            print(length * length <= input[1] * input[1] + input[2] * input[2] ? "DA" : "NE")
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ5575()
+let main = BOJ3034()
 main.run()
