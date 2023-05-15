ca39c74e73fba6453794925967ace45a83fe3310
taek0622 <min4644@gmail.com>
2023-05-15 19:27:31 +0900

    BOJ2355: 시그마
    - 수학, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/2355.swift
@@ -0,0 +1,16 @@
+//
+//  2355.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/05/15.
+//
+
+// 문제: https://www.acmicpc.net/problem/2355
+
+class BOJ2355: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 136B
+        let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+        print((input[0] + input[1]) * (abs(input[1] - input[0]) + 1) / 2)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ3034()
+let main = BOJ2355()
 main.run()
