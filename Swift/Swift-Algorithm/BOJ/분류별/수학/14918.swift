d7e2b61b8e95c0f81d6ba6d178904bdb2c8f163e
taek0622 <min4644@gmail.com>
2023-05-17 17:28:03 +0900

    BOJ14918: 더하기
    - 수학, 구현, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/14918.swift
@@ -0,0 +1,15 @@
+//
+//  14918.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/05/17.
+//
+
+// 문제: https://www.acmicpc.net/problem/14918
+
+class BOJ14918: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 78B
+        print(readLine()!.split(separator: " ").map { Int(String($0))! }.reduce(0, +))
+    }
+}
