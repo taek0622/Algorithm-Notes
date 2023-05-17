eff44bf7f09554eb70212e89660f695c547dfd6f
taek0622 <min4644@gmail.com>
2023-05-17 17:21:52 +0900

    BOJ4504: 배수 찾기
    - 수학, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/4504.swift
@@ -0,0 +1,19 @@
+//
+//  4504.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/05/17.
+//
+
+// 문제: https://www.acmicpc.net/problem/4504
+
+class BOJ4504: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 147B
+        let n = Int(readLine()!)!
+
+        while let num = readLine(), num != "0" {
+            print("\(num) is \(Int(num)! % n == 0 ? "" : "NOT ")a multiple of \(n).")
+        }
+    }
+}
