4d953204875e52d332a9c6f2e101dae415918b77
taek0622 <min4644@gmail.com>
2023-05-20 14:56:24 +0900

    BOJ10178: 할로윈의 사탕
    - 수학, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/10178.swift
@@ -0,0 +1,20 @@
+//
+//  10178.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/05/20.
+//
+
+// 문제: https://www.acmicpc.net/problem/10178
+
+class BOJ10178: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 224B
+        let T = Int(readLine()!)!
+
+        for _ in 0..<T {
+            let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+            print("You get \(input[0] / input[1]) piece(s) and your dad gets \(input[0] % input[1]) piece(s).")
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ1408()
+let main = BOJ10178()
 main.run()
