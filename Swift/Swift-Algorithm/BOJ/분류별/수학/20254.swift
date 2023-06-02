f892a23f0928b780d9fda4ab3a9453ae03bc7d0b
taek0622 <min4644@gmail.com>
2023-06-02 23:38:29 +0900

    BOJ20254: Site Score
    - 수학, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/20254.swift
@@ -0,0 +1,16 @@
+//
+//  20254.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/06/02.
+//
+
+// 문제: https://www.acmicpc.net/problem/20254
+
+class BOJ20254: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 138B
+        let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+        print(56 * input[0] + 24 * input[1] + 14 * input[2] + 6 * input[3])
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ18301()
+let main = BOJ20254()
 main.run()
