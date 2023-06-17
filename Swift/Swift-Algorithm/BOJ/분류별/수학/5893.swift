6e0ad444f98d6c08a9a899978c7a92a533234ce1
taek0622 <min4644@gmail.com>
2023-06-17 13:36:17 +0900

    BOJ5893: 17배
    - 수학, 구현, 임의 정밀도 / 큰 수 연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/5893.swift
@@ -0,0 +1,32 @@
+//
+//  5893.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/06/17.
+//
+
+// 문제: https://www.acmicpc.net/problem/5893
+
+class BOJ5893: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 413B
+        var N = Array(readLine()!.map { Int(String($0))! })
+        var result = [0] + N + Array(repeating: 0, count: 4)
+        N = Array(repeating: 0, count: 5) + N
+
+        result.reverse()
+        N.reverse()
+
+        for idx in 0..<result.count - 1 {
+            let number = N[idx] + result[idx]
+            result[idx] = number % 2
+            result[idx + 1] += number / 2
+        }
+
+        if result.last == 0 {
+            result.removeLast()
+        }
+
+        print(result.reversed().map { String($0) }.joined())
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ11023()
+let main = BOJ5893()
 main.run()
