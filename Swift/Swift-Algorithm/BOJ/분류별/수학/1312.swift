5977011ecf87534135eb9432f59b68b7cfa2f506
taek0622 <min4644@gmail.com>
2023-06-19 13:53:11 +0900

    BOJ1312: 소수
    - 수학

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/1312.swift
@@ -0,0 +1,23 @@
+//
+//  1312.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/06/19.
+//
+
+// 문제: https://www.acmicpc.net/problem/1312
+
+class BOJ1312: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 16ms, 코드 길이: 164B
+        let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var a = input[0]
+        let b = input[1]
+
+        for _ in 0..<input[2] {
+            a = a % b * 10
+        }
+
+        print(a / b)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ5618()
+let main = BOJ1312()
 main.run()
