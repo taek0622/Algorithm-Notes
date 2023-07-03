f30d8ecf8a69234d42a6cd79de8a62718b7b11a7
taek0622 <min4644@gmail.com>
2023-07-03 16:25:22 +0900

    BOJ2163: 초콜릿 자르기
    - 수학, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/2163.swift
@@ -0,0 +1,16 @@
+//
+//  2163.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/07/03.
+//
+
+// 문제: https://www.acmicpc.net/problem/2163
+
+class BOJ2163: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 100B
+        let input = readLine()!.split(separator: " ").map { Int(String($0))! }.reduce(1, *)
+        print(input - 1)
+    }
+}
