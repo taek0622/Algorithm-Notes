fef9fe10a0eb18b8c57710a0190ddfa78e800aa7
taek0622 <min4644@gmail.com>
2023-07-05 23:41:44 +0900

    BOJ7891: Can you add this?
    - 수학, 구현, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/7891.swift
@@ -0,0 +1,19 @@
+//
+//  7891.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/07/05.
+//
+
+// 문제: https://www.acmicpc.net/problem/7891
+
+class BOJ7891: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 128B
+        let T = Int(readLine()!)!
+
+        for _ in 0..<T {
+            print(readLine()!.split(separator: " ").map { Int(String($0))! }.reduce(0, +))
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ9655()
+let main = BOJ7891()
 main.run()
