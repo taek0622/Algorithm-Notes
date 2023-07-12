c5c27d67e9644c443c455e50a23d71f06996833e
taek0622 <min4644@gmail.com>
2023-07-12 18:15:08 +0900

    BOJ2783: 삼각 김밥
    - 수학, 구현, 사칙연산

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/2783.swift
@@ -0,0 +1,28 @@
+//
+//  2783.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/07/12.
+//
+
+// 문제: https://www.acmicpc.net/problem/2783
+
+class BOJ2783: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 366B
+        let seven = readLine()!.split(separator: " ").map { Double(String($0))! }
+        let N = Int(readLine()!)!
+        var minPrice = 1000 / seven[1] * seven[0]
+
+        for _ in 0..<N {
+            let input = readLine()!.split(separator: " ").map { Double(String($0))! }
+            let newPrice = 1000 / input[1] * input[0]
+
+            if newPrice < minPrice {
+                minPrice = newPrice
+            }
+        }
+
+        print(minPrice)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ11816()
+let main = BOJ2783()
 main.run()
