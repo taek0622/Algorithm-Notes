88dda50052ff90b662e5877c526687139dc691cc
taek0622 <min4644@gmail.com>
2023-07-14 18:54:30 +0900

    BOJ5347: LCM
    - 수학, 정수론, 유클리드 호제법

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/5347.swift
@@ -0,0 +1,28 @@
+//
+//  5347.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/07/14.
+//
+
+// 문제: https://www.acmicpc.net/problem/5347
+
+class BOJ5347: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 389B
+        let n = Int(readLine()!)!
+
+        for _ in 0..<n {
+            let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+            print(input[0] * input[1] / generateGCD(input[0], input[1]))
+        }
+
+        func generateGCD(_ baseNumber: Int, _ targetNumber: Int) -> Int {
+            if targetNumber % baseNumber == 0 {
+                return baseNumber
+            }
+
+            return generateGCD(targetNumber % baseNumber, baseNumber)
+        }
+    }
+}
