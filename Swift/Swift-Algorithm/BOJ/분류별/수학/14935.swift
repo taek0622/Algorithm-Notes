ec6ff049671b0fd053aaf91e15347a7b681c78fc
taek0622 <min4644@gmail.com>
2023-07-15 15:57:15 +0900

    BOJ14935: FA
    - 수학, 구현, 애드 혹

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/14935.swift
@@ -0,0 +1,16 @@
+//
+//  14935.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/07/15.
+//
+
+// 문제: https://www.acmicpc.net/problem/14935
+
+class BOJ14935: Solvable {
+    func run() {
+        // 메모리: 69092KB, 시간: 8ms
+        let _ = readLine()
+        print("FA")
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ5347()
+let main = BOJ14935()
 main.run()
