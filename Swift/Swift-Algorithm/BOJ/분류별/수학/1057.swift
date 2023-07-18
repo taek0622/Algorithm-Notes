cd90a4a9b0263aef4f8b25d2d9c2e7f4b63583cc
taek0622 <min4644@gmail.com>
2023-07-18 21:58:15 +0900

    BOJ1057: 토너먼트
    - 수학, 브루트포스 알고리즘

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/1057.swift
@@ -0,0 +1,46 @@
+//
+//  1057.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/07/18.
+//
+
+// 문제: https://www.acmicpc.net/problem/1057
+
+class BOJ1057: Solvable {
+    func run() {
+        solution2()
+    }
+
+    // 메모리: 69104KB, 시간: 8ms, 코드 길이: 328B
+    private func solution1() {
+        var input = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var currentRound = 1
+
+        while nextRound(input[1]) != nextRound(input[2]) {
+            input[1] = nextRound(input[1])
+            input[2] = nextRound(input[2])
+            currentRound += 1
+        }
+
+        print(currentRound)
+
+        func nextRound(_ number: Int) -> Int {
+            return (number + 1) / 2
+        }
+    }
+
+    // 메모리: 69104KB, 시간: 8ms, 코드 길이: 185B
+    private func solution2() {
+        var input = readLine()!.split(separator: " ").map { Int(String($0))! - 1 }
+        var round = 0
+
+        while input[1] != input[2] {
+            input[1] /= 2
+            input[2] /= 2
+            round += 1
+        }
+
+        print(round)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ2740()
+let main = BOJ1057()
 main.run()
