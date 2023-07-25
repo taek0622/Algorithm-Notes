ea3249eec65625d783073b54f4cb30d40731472d
taek0622 <min4644@gmail.com>
2023-07-25 23:39:17 +0900

    BOJ16395: 파스칼의 삼각형
    - 수학, 다이나믹 프로그래밍, 조합론

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/16395.swift
@@ -0,0 +1,30 @@
+//
+//  16395.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/07/25.
+//
+
+// 문제: https://www.acmicpc.net/problem/16395
+
+class BOJ16395: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 466B
+        let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var triangle = Array(repeating: Array(repeating: 0, count: input[0] + 1), count: input[0] + 1)
+        print(combination(input[0], input[1]))
+
+        func combination(_ n: Int, _ k: Int) -> Int {
+            if k == 1 || n == k {
+                return 1
+            }
+
+            if triangle[n][k] != 0 {
+                return triangle[n][k]
+            }
+
+            triangle[n][k] = combination(n - 1, k - 1) + combination(n - 1, k)
+            return triangle[n][k]
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ10158()
+let main = BOJ16395()
 main.run()
