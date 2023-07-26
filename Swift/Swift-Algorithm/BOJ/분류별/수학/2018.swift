ba7dda06b7e9016b824691a6777bde16cc91ce54
taek0622 <min4644@gmail.com>
2023-07-26 19:07:46 +0900

    BOJ2018: 수들의 합 5
    - 수학, 두 포인터

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/2018.swift
@@ -0,0 +1,35 @@
+//
+//  2018.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/07/26.
+//
+
+// 문제: https://www.acmicpc.net/problem/2018
+
+class BOJ2018: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 96ms, 코드 길이: 355B
+        let N = Int(readLine()!)!
+        var count = 1
+
+        if N != 1 {
+            for point1 in 1...(N/2) {
+                var sum = point1
+
+                for point2 in (point1+1)...(N/2+1) {
+                    sum += point2
+
+                    if sum == N {
+                        count += 1
+                        break
+                    } else if sum > N {
+                        break
+                    }
+                }
+            }
+        }
+
+        print(count)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ16395()
+let main = BOJ2018()
 main.run()
