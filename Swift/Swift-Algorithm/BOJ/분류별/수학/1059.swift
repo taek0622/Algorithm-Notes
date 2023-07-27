5cca089fe6e3a0a234b4bf3ee8fc50348651f343
taek0622 <min4644@gmail.com>
2023-07-27 16:57:24 +0900

    BOJ1059: 좋은 구간
    - 수학, 브루트포스 알고리즘, 정렬

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/1059.swift
@@ -0,0 +1,44 @@
+//
+//  1059.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/07/27.
+//
+
+// 문제: https://www.acmicpc.net/problem/1059
+
+class BOJ1059: Solvable {
+    func run() {
+        // 메모리: 69108KB, 시간: 8ms, 코드 길이: 585B
+        let L = Int(readLine()!)!
+        let S = readLine()!.split(separator: " ").map { Int(String($0))! }.sorted()
+        let n = Int(readLine()!)!
+        var numbers = [Int]()
+        var count = 0
+
+        for idx in 0..<S.count - 1 {
+            if S[idx]...S[idx + 1] ~= n {
+                numbers = Array(S[idx]...S[idx + 1])
+                break
+            }
+        }
+
+        if numbers.isEmpty {
+            numbers = Array(1..<S[0])
+        } else {
+            numbers.removeFirst()
+            numbers.removeLast()
+        }
+
+
+        for start in numbers.indices {
+            for end in start+1..<numbers.count {
+                if numbers[start]...numbers[end] ~= n {
+                    count += 1
+                }
+            }
+        }
+
+        print(count)
+    }
+}
