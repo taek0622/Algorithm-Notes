146971520cfadc8a2107272803078feacd880b2a
taek0622 <min4644@gmail.com>
2023-07-29 15:21:42 +0900

    BOJ9461: 파도반 수열
    - 수학, 다이나믹 프로그래밍

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/9461.swift
@@ -0,0 +1,44 @@
+//
+//  9461.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/07/29.
+//
+
+// 문제: https://www.acmicpc.net/problem/9461
+
+class BOJ9461: Solvable {
+    func run() {
+        solution2()
+    }
+
+    // 메모리: 69100KB, 시간: 8ms, 코드 길이: 175B
+    private func solution1() {
+        var P = [1, 1, 1, 2, 2]
+        let T = Int(readLine()!)!
+
+        for idx in 5...100 {
+            P.append(P[idx-1] + P[idx-5])
+        }
+
+        for _ in 0..<T {
+            let N = Int(readLine()!)!
+            print(P[N-1])
+        }
+    }
+
+    // 메모리: 69100KB, 시간: 8ms, 코드 길이: 169B
+    private func solution2() {
+        var P = [1, 1, 1]
+        let T = Int(readLine()!)!
+
+        for idx in 3...100 {
+            P.append(P[idx-3] + P[idx-2])
+        }
+
+        for _ in 0..<T {
+            let N = Int(readLine()!)!
+            print(P[N-1])
+        }
+    }
+}
