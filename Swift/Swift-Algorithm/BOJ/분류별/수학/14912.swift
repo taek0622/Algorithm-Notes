73d4d96d00e8c0de382e6438aa4084353fa7d683
taek0622 <min4644@gmail.com>
2023-08-04 16:20:52 +0900

    BOJ14912: 숫자 빈도수
    - 수학, 구현, 브루트포스 알고리즘

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/14912.swift
@@ -0,0 +1,46 @@
+//
+//  14912.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/08/04.
+//
+
+// 문제: https://www.acmicpc.net/problem/14912
+
+class BOJ14912: Solvable {
+    func run() {
+        solution2()
+    }
+
+    // 메모리: 69104KB, 시간: 64ms, 코드 길이: 209B
+    private func solution1() {
+        let input = readLine()!.split(separator: " ").map { String($0) }
+        var count = 0
+
+        for number in 1...Int(input[0])! {
+            count += String(number).map { String($0) }.filter { $0 == input[1] }.count
+        }
+
+        print(count)
+    }
+
+    // 메모리: 69104KB, 시간: 12ms, 코드 길이: 264B
+    private func solution2() {
+        let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var count = 0
+
+        for number in 1...input[0] {
+            var num = number
+
+            while num > 0 {
+                if num % 10 == input[1] {
+                    count += 1
+                }
+
+                num /= 10
+            }
+        }
+
+        print(count)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ3474()
+let main = BOJ14912()
 main.run()
