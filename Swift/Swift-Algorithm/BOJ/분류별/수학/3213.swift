437f9f68cd43e2e3cecf9ef7c13f120b901ca5cf
taek0622 <min4644@gmail.com>
2023-08-20 21:18:32 +0900

    BOJ3213: 피자
    - 수학, 그리디 알고리즘

---
+++ Swift/Swift-Algorithm/BOJ/분류별/수학/3213.swift
@@ -0,0 +1,64 @@
+//
+//  3213.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/08/20.
+//
+
+// 문제: https://www.acmicpc.net/problem/3213
+
+class BOJ3213: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 12ms, 코드 길이: 770B
+        let N = Int(readLine()!)!
+        var pizza = 0
+        var slices = (0, 0, 0)
+
+        for _ in 0..<N {
+            switch readLine() {
+            case "1/4":
+                slices.0 += 1
+            case "1/2":
+                slices.1 += 1
+            case "3/4":
+                slices.2 += 1
+            default:
+                break
+            }
+        }
+
+        pizza += slices.1 / 2
+        slices.1 -= pizza * 2
+
+        var sum = min(slices.0, slices.2)
+        pizza += sum
+        slices.0 -= sum
+        slices.2 -= sum
+
+        sum = min(slices.0 / 2, slices.1)
+        pizza += sum
+        slices.0 -= sum * 2
+        slices.1 -= sum
+
+        sum = min(slices.0, slices.1)
+        pizza += sum
+        slices.0 -= sum
+        slices.2 -= sum
+
+        sum = slices.0 / 4
+        pizza += sum
+        slices.0 -= sum * 4
+
+        sum = slices.0 / 3
+        pizza += sum
+        slices.0 -= sum * 3
+
+        sum = slices.0 / 2
+        pizza += sum
+        slices.0 -= sum * 2
+
+        pizza += (slices.0 + slices.1 + slices.2)
+
+        print(pizza)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ17252()
+let main = BOJ3213()
 main.run()
