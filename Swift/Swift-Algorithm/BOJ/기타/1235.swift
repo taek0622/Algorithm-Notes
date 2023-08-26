7779248a76732053d8253abeecdec8b2c4a3b253
taek0622 <min4644@gmail.com>
2023-08-26 21:10:03 +0900

    BOJ1235: 학생 번호
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/기타/1235.swift
@@ -0,0 +1,68 @@
+//
+//  1235.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/08/26.
+//
+
+// 문제: https://www.acmicpc.net/problem/1235
+
+class BOJ1235: Solvable {
+    func run() {
+        solution2()
+    }
+
+    // 메모리: 70428KB, 시간: 20ms, 코드 길이: 354B
+    private func solution1() {
+        let N = Int(readLine()!)!
+        var students = [[String]]()
+
+        for _ in 0..<N {
+            students.append(readLine()!.map { String($0) }.reversed())
+        }
+
+        var k = 0
+        while k < students[0].count {
+            var id = Set<String>()
+
+            for student in students {
+                id.insert(student[0...k].joined())
+            }
+
+            if id.count == N {
+                break
+            }
+
+            k += 1
+        }
+
+        print(k+1)
+    }
+
+    // 메모리: 69264KB, 시간: 12ms, 코드 길이: 322B
+    private func solution2() {
+        let N = Int(readLine()!)!
+        var students = [String]()
+
+        for _ in 0..<N {
+            students.append(readLine()!)
+        }
+
+        var k = 0
+        while k < students[0].count {
+            var id = Set<String>()
+
+            for student in students {
+                id.insert(String(student.suffix(k)))
+            }
+
+            if id.count == N {
+                break
+            }
+
+            k += 1
+        }
+
+        print(k)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ1158()
+let main = BOJ1235()
 main.run()
