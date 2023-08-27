eedb0fd1db2a7deda34052146825f2759f14f0f9
taek0622 <min4644@gmail.com>
2023-08-28 00:20:42 +0900

    BOJ2557: Hello World
    - 구현
    - 폴더 정리

--- Swift/Swift-Algorithm/BOJ/2557.swift
+++ Swift/Swift-Algorithm/BOJ/2557.swift
@@ -5,7 +5,10 @@
 //  Created by 김민택 on 2022/05/10.
 //
 
-// 문제: https://www.acmicpc.net/problem/2557
+/*
+ 링크: https://www.acmicpc.net/problem/2557
+ 알고리즘 분류: 구현
+ */
 
 class BOJ2557: Solvable {
     func run() {
