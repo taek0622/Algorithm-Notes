fe40136548b6685d71b00002d5790ddaef26954a
taek0622 <min4644@gmail.com>
2023-08-28 01:08:28 +0900

    BOJ2420: 사파리월드
    - 수학, 구현, 사칙연산

--- Swift/Swift-Algorithm/BOJ/2420.swift
+++ Swift/Swift-Algorithm/BOJ/2420.swift
@@ -5,7 +5,8 @@
 //  Created by 김민택 on 2023/01/24.
 //
 
-// 문제: https://www.acmicpc.net/problem/2420
+//  문제 링크: https://www.acmicpc.net/problem/2420
+//  알고리즘 분류: 수학, 구현, 사칙연산
 
 class BOJ2420: Solvable {
     func run() {
