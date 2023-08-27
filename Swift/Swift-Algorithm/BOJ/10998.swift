a617657374d5bd558997a59185e4ed4c61449fe2
taek0622 <min4644@gmail.com>
2023-08-28 00:36:15 +0900

    BOJ10998: A×B
    - 수학, 구현, 사칙연산

--- Swift/Swift-Algorithm/BOJ/10998.swift
+++ Swift/Swift-Algorithm/BOJ/10998.swift
@@ -5,7 +5,8 @@
 //  Created by 김민택 on 2022/05/10.
 //
 
-// 문제: https://www.acmicpc.net/problem/10998
+//  문제 링크: https://www.acmicpc.net/problem/10998
+//  알고리즘 분류: 수학, 구현, 사칙연산
 
 class BOJ10998: Solvable {
     func run() {
