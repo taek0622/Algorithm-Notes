d98a78358af3429d4f7c1a50bc92ed7cce7da6b1
taek0622 <min4644@gmail.com>
2023-08-28 01:09:50 +0900

    BOJ2743: 단어 길이 재기
    - 구현, 문자열

--- Swift/Swift-Algorithm/BOJ/2743.swift
+++ Swift/Swift-Algorithm/BOJ/2743.swift
@@ -5,7 +5,8 @@
 //  Created by 김민택 on 2023/01/24.
 //
 
-// 문제: https://www.acmicpc.net/problem/2743
+//  문제 링크: https://www.acmicpc.net/problem/2743
+//  알고리즘 분류: 구현, 문자열
 
 class BOJ2743: Solvable {
     func run() {
