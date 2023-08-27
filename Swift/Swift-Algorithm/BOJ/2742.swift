0be28172bf126a1280ad704e542221e9f55e67de
taek0622 <min4644@gmail.com>
2023-08-28 01:19:12 +0900

    BOJ2742: 기찍 N
    - 구현

--- Swift/Swift-Algorithm/BOJ/2742.swift
+++ Swift/Swift-Algorithm/BOJ/2742.swift
@@ -5,7 +5,8 @@
 //  Created by 김민택 on 2022/05/16.
 //
 
-// 문제: https://www.acmicpc.net/problem/2742
+//  문제 링크: https://www.acmicpc.net/problem/2742
+//  알고리즘 분류: 구현
 
 class BOJ2742: Solvable {
     func run() {
