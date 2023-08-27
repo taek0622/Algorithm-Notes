c1e6192943827b3245602e36558041d630ee2eee
taek0622 <min4644@gmail.com>
2023-08-28 01:10:58 +0900

    BOJ2754: 학점계산
    - 구현, 문자열

--- Swift/Swift-Algorithm/BOJ/2754.swift
+++ Swift/Swift-Algorithm/BOJ/2754.swift
@@ -5,7 +5,8 @@
 //  Created by 김민택 on 2023/01/24.
 //
 
-// 문제: https://www.acmicpc.net/problem/2754
+//  문제 링크: https://www.acmicpc.net/problem/2754
+//  알고리즘 분류: 구현, 문자열
 
 class BOJ2754: Solvable {
     func run() {
