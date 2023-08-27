0caf77d7ecb64c428bb13806c3aa12aa9c408967
taek0622 <min4644@gmail.com>
2023-08-28 00:32:10 +0900

    BOJ10171: 고양이
    - 구현

--- Swift/Swift-Algorithm/BOJ/10171.swift
+++ Swift/Swift-Algorithm/BOJ/10171.swift
@@ -5,7 +5,8 @@
 //  Created by 김민택 on 2022/05/10.
 //
 
-// 문제: https://www.acmicpc.net/problem/10171
+//  문제 링크: https://www.acmicpc.net/problem/10171
+//  알고리즘 분류: 구현
 
 class BOJ10171: Solvable {
     func run() {
