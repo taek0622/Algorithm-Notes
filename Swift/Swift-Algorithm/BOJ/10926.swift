6dc04a4b10cbcb0312671a3941a51bd462c00abf
taek0622 <min4644@gmail.com>
2023-08-28 00:35:17 +0900

    BOJ10926: ??!
    - 구현

--- Swift/Swift-Algorithm/BOJ/10926.swift
+++ Swift/Swift-Algorithm/BOJ/10926.swift
@@ -5,7 +5,8 @@
 //  Created by 김민택 on 2022/05/11.
 //
 
-// 문제: https://www.acmicpc.net/problem/10926
+//  문제 링크: https://www.acmicpc.net/problem/10926
+//  알고리즘 분류: 구현
 
 class BOJ10926: Solvable {
     func run() {
