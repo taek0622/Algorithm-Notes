49126dfd68c4a521d36d21547035dcaeff1c1d7f
taek0622 <min4644@gmail.com>
2023-08-28 00:32:46 +0900

    BOJ10172: 개
    - 구현

--- Swift/Swift-Algorithm/BOJ/10172.swift
+++ Swift/Swift-Algorithm/BOJ/10172.swift
@@ -5,7 +5,8 @@
 //  Created by 김민택 on 2022/12/25.
 //
 
-// 문제: https://www.acmicpc.net/problem/10172
+//  문제 링크: https://www.acmicpc.net/problem/10172
+//  알고리즘 분류: 구현
 
 class BOJ10172: Solvable {
     // 메모리: 69092KB, 시간: 8ms, 코드 길이: 80B
