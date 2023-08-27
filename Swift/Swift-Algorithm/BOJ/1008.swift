c355894085cf29294cf48c01e26e1519026d342a
taek0622 <min4644@gmail.com>
2023-08-28 00:28:17 +0900

    BOJ1008: A/B
    - 수학, 구현, 사칙연산

--- Swift/Swift-Algorithm/BOJ/1008.swift
+++ Swift/Swift-Algorithm/BOJ/1008.swift
@@ -5,7 +5,8 @@
 //  Created by 김민택 on 2022/05/10.
 //
 
-// 문제: https://www.acmicpc.net/problem/1008
+//  문제 링크: https://www.acmicpc.net/problem/1008
+//  알고리즘 분류: 수학, 구현, 사칙연산
 
 class BOJ1008: Solvable {
     func run() {
