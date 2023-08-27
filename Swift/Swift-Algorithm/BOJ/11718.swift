ee092fdaae8c4ed0d88c34755551249f3811368b
taek0622 <min4644@gmail.com>
2023-08-28 01:12:46 +0900

    BOJ11718: 그대로 출력하기
    - 구현, 문자열

--- Swift/Swift-Algorithm/BOJ/11718.swift
+++ Swift/Swift-Algorithm/BOJ/11718.swift
@@ -5,7 +5,8 @@
 //  Created by 김민택 on 2023/01/25.
 //
 
-// 문제: https://www.acmicpc.net/problem/11718
+//  문제 링크: https://www.acmicpc.net/problem/11718
+//  알고리즘 분류: 구현, 문자열
 
 class BOJ11718: Solvable {
     func run() {
