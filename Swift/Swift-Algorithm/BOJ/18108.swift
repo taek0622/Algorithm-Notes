1cbbc6bc1c6ff52312df035610a07007ff517da8
taek0622 <min4644@gmail.com>
2023-08-28 00:37:33 +0900

    BOJ18108: 1998년생인 내가 태국에서는 2541년생?!
    - 수학, 사칙연산

--- Swift/Swift-Algorithm/BOJ/18108.swift
+++ Swift/Swift-Algorithm/BOJ/18108.swift
@@ -5,7 +5,8 @@
 //  Created by 김민택 on 2022/05/11.
 //
 
-// 문제: https://www.acmicpc.net/problem/18108
+//  문제 링크: https://www.acmicpc.net/problem/18108
+//  알고리즘 분류: 수학, 사칙연산
 
 class BOJ18108: Solvable {
     func run() {
