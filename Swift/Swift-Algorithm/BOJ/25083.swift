42d1ca880c8806eb5554a9f71a1a8b8b7020ab0c
taek0622 <min4644@gmail.com>
2023-08-28 00:38:53 +0900

    BOJ25083: 새싹
    - 구현

--- Swift/Swift-Algorithm/BOJ/25083.swift
+++ Swift/Swift-Algorithm/BOJ/25083.swift
@@ -5,7 +5,8 @@
 //  Created by 김민택 on 2022/05/10.
 //
 
-// 문제: https://www.acmicpc.net/problem/25083
+//  문제 링크: https://www.acmicpc.net/problem/25083
+//  알고리즘 분류: 구현
 
 class BOJ25083: Solvable {
     func run() {
