3cf9f3bf24e9b84c598f4e99d21c9fd8f408a847
taek0622 <min4644@gmail.com>
2023-09-05 16:33:02 +0900

    BOJ1758: 알바생 강호
    - 그리디 알고리즘, 정렬

---
+++ Swift/Swift-Algorithm/BOJ/1758.swift
@@ -0,0 +1,30 @@
+//
+//  1758.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/09/05.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/1758
+//  알고리즘 분류: 그리디 알고리즘, 정렬
+
+class BOJ1758: Solvable {
+    func run() {
+        // 메모리: 70672KB, 시간: 56ms, 코드 길이: 232B
+        let N = Int(readLine()!)!
+        var tips = [Int]()
+        var result = 0
+
+        for _ in 0..<N {
+            tips.append(Int(readLine()!)!)
+        }
+
+        tips.sort(by: >)
+
+        for idx in tips.indices where tips[idx] - idx > 0 {
+            result += tips[idx] - idx
+        }
+
+        print(result)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ1755()
+let main = BOJ1758()
 main.run()
