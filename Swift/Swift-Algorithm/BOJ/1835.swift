e818365127ae801741b75ccb88d54a10591e6e29
taek0622 <min4644@gmail.com>
2023-09-07 20:38:05 +0900

    BOJ1835: 카드
    - 구현, 자료 구조, 시뮬레이션, 덱

---
+++ Swift/Swift-Algorithm/BOJ/1835.swift
@@ -0,0 +1,30 @@
+//
+//  1835.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/09/07.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/1835
+//  알고리즘 분류: 구현, 자료 구조, 시뮬레이션, 덱
+
+class BOJ1835: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 44ms, 코드 길이: 284B
+        let N = Int(readLine()!)!
+        var cards = Array(1...N)
+        var origin = [Int]()
+
+        for num in 1...N {
+            for _ in 0..<num {
+                cards.append(cards.removeFirst())
+            }
+
+            origin.append(cards.removeFirst())
+        }
+
+        for num in 1...N {
+            print(origin.firstIndex(of: num)!+1, terminator: " ")
+        }
+    }
+}
