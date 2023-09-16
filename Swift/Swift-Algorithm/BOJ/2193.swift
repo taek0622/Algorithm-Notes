4ccd5f269ccf22487d0edc784e9474df75d2d158
taek0622 <min4644@gmail.com>
2023-09-16 23:14:17 +0900

    BOJ2193: 이친수
    - 다이나믹 프로그래밍

---
+++ Swift/Swift-Algorithm/BOJ/2193.swift
@@ -0,0 +1,29 @@
+//
+//  2193.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/09/17.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/2193
+//  알고리즘 분류: 다이나믹 프로그래밍
+
+class BOJ2193: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 252B
+        let N = Int(readLine()!)!
+
+        if 1...2 ~= N {
+            print(1)
+        } else {
+            var pinary = Array(repeating: 0, count: N)
+            pinary[0...1] = [1, 1]
+
+            for idx in 2...N-1 {
+                pinary[idx] = pinary[idx - 1] + pinary[idx - 2]
+            }
+
+            print(pinary[N-1])
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ2012()
+let main = BOJ2193()
 main.run()
