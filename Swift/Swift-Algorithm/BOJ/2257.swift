610c88f8056d7fd4ce976945bf8f3c6755989076
taek0622 <min4644@gmail.com>
2023-09-19 23:26:33 +0900

    BOJ2257: 화학식량
    - 자료 구조, 문자열, 스택

---
+++ Swift/Swift-Algorithm/BOJ/2257.swift
@@ -0,0 +1,44 @@
+//
+//  2257.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/09/19.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/2257
+//  알고리즘 분류: 자료 구조, 문자열, 스택
+
+class BOJ2257: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 535B
+        let input = readLine()!.map { String($0) }
+        var stack = [Int]()
+
+        for element in input {
+            switch element {
+            case "(":
+                stack.append(0)
+            case ")":
+                var temp = 0
+
+                while stack.last != 0 {
+                    temp += stack.popLast()!
+                }
+
+                stack.removeLast()
+
+                stack.append(temp)
+            case "H":
+                stack.append(1)
+            case "C":
+                stack.append(12)
+            case "O":
+                stack.append(16)
+            default:
+                stack.append(stack.popLast()! * Int(element)!)
+            }
+        }
+
+        print(stack.reduce(0, +))
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ2178()
+let main = BOJ2257()
 main.run()
