c34637136bc00c2d0af6db05be5277a180f71978
taek0622 <min4644@gmail.com>
2023-10-05 22:36:01 +0900

    BOJ2805: 나무 자르기
    - 이분 탐색, 매개 변수 탐색

---
+++ Swift/Swift-Algorithm/BOJ/2805.swift
@@ -0,0 +1,47 @@
+//
+//  2805.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 10/5/23.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/2805
+//  알고리즘 분류: 이분 탐색, 매개 변수 탐색
+
+class BOJ2805: Solvable {
+    func run() {
+        // 메모리: 145408KB, 시간: 580ms, 코드 길이: 625B
+        let NM = readLine()!.split(separator: " ").map { Int($0)! }
+        let tree = readLine()!.split(separator: " ").map { Int($0)! }
+        var visited = Set<Int>()
+        var (left, right) = (0, tree.max()!)
+        var mid = right / 2
+
+        while true {
+            var sum = 0
+
+            for idx in 0..<NM[0] where tree[idx] >  mid {
+                sum += tree[idx] - mid
+            }
+
+            if sum == NM[1] {
+                break
+            } else if sum > NM[1] {
+                if !visited.insert(mid+1).inserted {
+                    break
+                }
+
+                left = mid
+            } else if mid == 0 {
+                break
+            } else {
+                right = mid
+            }
+
+            visited.insert(mid)
+            mid = (left + right) / 2
+        }
+
+        print(mid)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ11727()
+let main = BOJ2805()
 main.run()
