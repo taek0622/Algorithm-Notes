836714dc778cca3caed9d8bba45ee0605269acaa
taek0622 <min4644@gmail.com>
2023-10-07 21:17:19 +0900

    BOJ10844: 쉬운 계단 수
    - 다이나믹 프로그래밍

---
+++ Swift/Swift-Algorithm/BOJ/10844.swift
@@ -0,0 +1,29 @@
+//
+//  10844.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 10/7/23.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/10844
+//  알고리즘 분류: 다이나믹 프로그래밍
+
+class BOJ10844: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 278B
+        let N = Int(readLine()!)!
+        var dp = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+        for _ in 1..<N {
+            var temp = [dp[1], 0, 0, 0, 0, 0, 0, 0, 0, dp[8]]
+
+            for idx in 1...8 {
+                temp[idx] = (dp[idx-1] + dp[idx+1]) % 1000000000
+            }
+
+            dp = temp
+        }
+
+        print(dp.reduce(0, +) % 1000000000)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ1966()
+let main = BOJ10844()
 main.run()
