9fca501e44449c95cfd318379ddff5848aa33079
taek0622 <min4644@gmail.com>
2023-10-18 16:20:42 +0900

    BOJ1149: RGB거리
    - 다이나믹 프로그래밍

---
+++ Swift/Swift-Algorithm/BOJ/1149.swift
@@ -0,0 +1,24 @@
+//
+//  1149.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 10/18/23.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/1149
+//  알고리즘 분류: 다이나믹 프로그래밍
+
+class BOJ1149: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 306B
+        let N = Int(readLine()!)!
+        var dp = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+        for _ in 1..<N {
+            let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+            dp = [input[0] + min(dp[1], dp[2]), input[1] + min(dp[0], dp[2]), input[2] + min(dp[0], dp[1])]
+        }
+
+        print(dp.min()!)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ7569()
+let main = BOJ1149()
 main.run()
