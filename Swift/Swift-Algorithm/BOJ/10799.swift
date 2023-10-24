c243cc3f5de01541f0f532d25260244495732b73
taek0622 <min4644@gmail.com>
2023-10-24 19:08:17 +0900

    BOJ10799: 쇠막대기
    - 자료 구조, 스택

---
+++ Swift/Swift-Algorithm/BOJ/10799.swift
@@ -0,0 +1,35 @@
+//
+//  10799.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 10/24/23.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/10799
+//  알고리즘 분류: 자료 구조, 스택
+
+class BOJ10799: Solvable {
+    func run() {
+        // 메모리: 70660KB, 시간: 16ms, 코드 길이: 390B
+        let brackets = readLine()!.map { String($0) }
+        var total = 0
+        var current = 0
+        var idx = 0
+
+        while idx < brackets.count {
+            if idx < brackets.count - 1 && brackets[idx] == "(" && brackets[idx+1] == ")" {
+                total += current
+                idx += 1
+            } else if brackets[idx] == "(" {
+                total += 1
+                current += 1
+            } else {
+                current -= 1
+            }
+
+            idx += 1
+        }
+
+        print(total)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ1182()
+let main = BOJ10799()
 main.run()
