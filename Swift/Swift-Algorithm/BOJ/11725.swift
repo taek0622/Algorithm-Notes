e391e965f775e84f5ec85f1dba42900a6604336d
taek0622 <min4644@gmail.com>
2023-10-25 23:18:36 +0900

    BOJ11725: 트리의 부모 찾기
    - 그래프 이론, 그래프 탐색, 트리, 너비 우선 탐색, 깊이 우선 탐색

---
+++ Swift/Swift-Algorithm/BOJ/11725.swift
@@ -0,0 +1,46 @@
+//
+//  11725.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 10/25/23.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/11725
+//  알고리즘 분류: 그래프 이론, 그래프 탐색, 트리, 너비 우선 탐색, 깊이 우선 탐색
+
+class BOJ11725: Solvable {
+    func run() {
+        // 메모리: 87120KB, 시간: 240ms, 코드 길이: 708B
+        let N = Int(readLine()!)!
+        var node = [Int: [Int]]()
+        var visited = Set<Int>()
+        var root = Array(repeating: 0, count: N+1)
+
+        for _ in 0..<N-1 {
+            let input = readLine()!.split(separator: " ").map { Int($0)! }
+            node[input[0], default: []].append(input[1])
+            node[input[1], default: []].append(input[0])
+        }
+
+        var queue = [1]
+        var idx = 0
+
+        while idx < queue.count {
+            let number = queue[idx]
+            idx += 1
+
+            if !visited.insert(number).inserted {
+                continue
+            }
+
+            let nextNodes = node[number, default: []].filter { root[$0] == 0 }
+            queue.append(contentsOf: nextNodes)
+
+            for next in nextNodes {
+                root[next] = number
+            }
+        }
+
+        print(root[2...].map { String($0) }.joined(separator: "\n"))
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ10799()
+let main = BOJ11725()
 main.run()
