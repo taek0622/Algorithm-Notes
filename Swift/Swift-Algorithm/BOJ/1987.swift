d59c1f6618d07bf7bbb88773c6e6b99a90f35473
taek0622 <min4644@gmail.com>
2023-10-31 21:35:16 +0900

    BOJ1987: 알파벳
    - 그래프 이론, 그래프 탐색, 깊이 우선 탐색, 백트래킹

---
+++ Swift/Swift-Algorithm/BOJ/1987.swift
@@ -0,0 +1,43 @@
+//
+//  1987.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 10/31/23.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/1987
+//  알고리즘 분류: 그래프 이론, 그래프 탐색, 깊이 우선 탐색, 백트래킹
+
+class BOJ1987: Solvable {
+    func run() {
+        // 메모리: 69108KB, 시간: 1976ms, 코드 길이: 634B
+        let RC = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var board = [[Int]]()
+        var count = 0
+
+        for _ in 0..<RC[0] {
+            board.append(readLine()!.map { Int($0.asciiValue!) - 65 })
+        }
+
+        dfs((0, 0), 1, 1 << board[0][0])
+
+        print(count)
+
+        func dfs(_ current: (Int, Int), _ depth: Int, _ visited: Int) {
+            count = max(count, depth)
+
+            let (x, y) = current
+
+            [(x-1, y), (x, y-1), (x, y+1), (x+1, y)].forEach {
+                if 0..<RC[0] ~= $0.0 && 0..<RC[1] ~= $0.1 {
+                    let next = 1 << board[$0.0][$0.1]
+
+                    if next & visited == 0 {
+                        dfs($0, depth + 1, next | visited)
+                    }
+                }
+            }
+        }
+    }
+}
+
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ1074()
+let main = BOJ1987()
 main.run()
