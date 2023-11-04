a58f115697ba53cc7795cc15bc2ad1327fb72ec7
taek0622 <min4644@gmail.com>
2023-11-05 03:39:12 +0900

    BOJ1406: 에디터
    - 자료 구조, 스택, 연결 리스트

---
+++ Swift/Swift-Algorithm/BOJ/1406.swift
@@ -0,0 +1,39 @@
+//
+//  1406.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 11/5/23.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/1406
+//  알고리즘 분류: 자료 구조, 스택, 연결 리스트
+
+class BOJ1406: Solvable {
+    func run() {
+        // 메모리: 95436KB, 시간: 264ms, 코드 길이: 483B
+        var left = readLine()!.map { $0 }
+        var right = [Character]()
+        let N = Int(readLine()!)!
+
+        for _ in 0..<N {
+            let command = readLine()!
+
+            switch command {
+            case "L":
+                if !left.isEmpty {
+                    right.append(left.removeLast())
+                }
+            case "D":
+                if !right.isEmpty {
+                    left.append(right.removeLast())
+                }
+            case "B":
+                _ = left.popLast()
+            default:
+                left.append(command.last!)
+            }
+        }
+
+        print(String(left + right.reversed()))
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ5430()
+let main = BOJ1406()
 main.run()
