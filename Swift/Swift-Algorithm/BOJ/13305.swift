63bad17227632000f9ad8e48e202472a5961afe6
taek0622 <min4644@gmail.com>
2023-11-10 23:08:52 +0900

    BOJ13305: 주유소
    - 그리디 알고리즘

---
+++ Swift/Swift-Algorithm/BOJ/13305.swift
@@ -0,0 +1,30 @@
+//
+//  13305.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 11/10/23.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/13305
+//  알고리즘 분류: 그리디 알고리즘
+
+class BOJ13305: Solvable {
+    func run() {
+        // 메모리: 79612KB, 시간: 88ms, 코드 길이: 361B
+        let N = Int(readLine()!)!
+        let distance = readLine()!.split(separator: " ").map { Int($0)! }
+        let price = readLine()!.split(separator: " ").map { Int($0)! }
+        var bestPrice = price[0]
+        var total = price[0] * distance[0]
+
+        for idx in 1..<N-1 {
+            if price[idx] < bestPrice {
+                bestPrice = price[idx]
+            }
+
+            total += bestPrice * distance[idx]
+        }
+
+        print(total)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ2583()
+let main = BOJ13305()
 main.run()
