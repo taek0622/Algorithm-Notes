49e3d7cea7646f338026d44d74aaf14e9da76bd9
taek0622 <min4644@gmail.com>
2023-11-24 23:00:25 +0900

    BOJ11660: 구간 합 구하기 5
    - 다이나믹 프로그래밍, 누적 합

---
+++ Swift/Swift-Algorithm/BOJ/11660.swift
@@ -0,0 +1,82 @@
+//
+//  11660.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 11/24/23.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/11660
+//  알고리즘 분류: 다이나믹 프로그래밍, 누적 합
+
+class BOJ11660: Solvable {
+    func run() {
+        solution1()
+    }
+
+    // 메모리: 79368KB, 시간: 532ms, 코드 길이: 1255B
+    private func solution1() {
+        let NM = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var numbers = Array(repeating: Array(repeating: 0, count: NM[0]), count: NM[0])
+        var result = ""
+
+        for row in 0..<NM[0] {
+            let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+            var rowArray = input
+
+            if row == 0 {
+                for col in 1..<NM[0] {
+                    rowArray[col] += rowArray[col-1]
+                }
+            } else {
+                rowArray[0] += numbers[row-1][0]
+
+                for col in 1..<NM[0] {
+                    rowArray[col] = rowArray[col] + numbers[row-1][col] + rowArray[col-1] - numbers[row-1][col-1]
+                }
+            }
+
+            numbers[row] = rowArray
+        }
+
+        for _ in 0..<NM[1] {
+            let input = readLine()!.split(separator: " ").map { Int(String($0))! - 1 }
+
+            if input[0] == 0 && input[1] == 0 {
+                result += "\(numbers[input[2]][input[3]])"
+            } else if input[0] == 0 {
+                result += "\(numbers[input[2]][input[3]] - numbers[input[2]][input[1]-1])"
+            } else if input[1] == 0 {
+                result += "\(numbers[input[2]][input[3]] - numbers[input[0]-1][input[3]])"
+            } else {
+                result += "\(numbers[input[2]][input[3]] - numbers[input[2]][input[1]-1] - numbers[input[0]-1][input[3]] + numbers[input[0]-1][input[1]-1])"
+            }
+
+            result += "\n"
+        }
+
+        print(result)
+    }
+
+    // 메모리: 79368KB, 시간: 556ms, 코드 길이: 691B
+    private func solution2() {
+        let NM = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var numbers = Array(repeating: Array(repeating: 0, count: NM[0] + 1), count: NM[0] + 1)
+        var result = ""
+
+        for row in 1...NM[0] {
+            numbers[row] = [0] + readLine()!.split(separator: " ").map { Int(String($0))! }
+
+            for col in 1...NM[0] {
+                numbers[row][col] = numbers[row][col] + numbers[row-1][col] + numbers[row][col-1] - numbers[row-1][col-1]
+            }
+        }
+
+        for _ in 0..<NM[1] {
+            let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+            result += "\(numbers[input[2]][input[3]] - numbers[input[2]][input[1]-1] - numbers[input[0]-1][input[3]] + numbers[input[0]-1][input[1]-1])\n"
+        }
+
+        print(result)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ11722()
+let main = BOJ11660()
 main.run()
