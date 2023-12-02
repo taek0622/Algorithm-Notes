82134fca6ba2c306b9e1d35797726090cd1fbb3e
taek0622 <min4644@gmail.com>
2023-12-02 23:19:19 +0900

    BOJ10819: 차이를 최대로
    - 브루트포스 알고리즘, 백트래킹

---
+++ Swift/Swift-Algorithm/BOJ/10819.swift
@@ -0,0 +1,35 @@
+//
+//  10819.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 12/2/23.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/10819
+//  알고리즘 분류: 브루트포스 알고리즘, 백트래킹
+
+class BOJ10819: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 28ms, 코드 길이: 441B
+        let N = Int(readLine()!)!
+        let A = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var result = Int.min
+
+        for idx in A.indices {
+            dfs([idx], 1, 0)
+        }
+
+        print(result)
+
+        func dfs(_ array: [Int], _ depth: Int, _ sum: Int) {
+            if depth == N {
+                result = max(result, sum)
+                return
+            }
+
+            for idx in A.indices where !array.contains(idx) {
+                dfs(array + [idx], depth + 1, sum + abs(A[array.last!] - A[idx]))
+            }
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ2206()
+let main = BOJ10819()
 main.run()
