4484406e78b2b0b700b1841d5ebcca7230979302
taek0622 <min4644@gmail.com>
2023-12-24 00:05:05 +0900

    BOJ1926: 그림 (modify solution)
    - 그래프 이론, 그래프 탐색, 너비 우선 탐색, 깊이 우선 탐색

--- Swift/Swift-Algorithm/BOJ/1926.swift
+++ Swift/Swift-Algorithm/BOJ/1926.swift
@@ -10,30 +10,30 @@
 
 class BOJ1926: Solvable {
     func run() {
-        // 메모리: 77432KB, 시간: 76ms, 코드 길이: 953B
-        let nm = readLine()!.split(separator: " ").map { Int(String($0))! }
-        var paper = Array(repeating: Array(repeating: 0, count: nm[1]), count: nm[0])
+        // 메모리: 75720KB, 시간: 64ms, 코드 길이: 955B
+        let nm = readLine()!.split(separator: " ").map { Int($0)! }
+        var paper = Array(repeating: Array(repeating: false, count: nm[1]), count: nm[0])
         var (count, maxWidth) = (0, 0)
 
         for idx in 0..<nm[0] {
-            paper[idx] = readLine()!.split(separator: " ").map { Int(String($0))! }
+            paper[idx] = readLine()!.split(separator: " ").map { $0 == "1" ? true : false }
         }
 
         for row in 0..<nm[0] {
-            for col in 0..<nm[1] where paper[row][col] == 1 {
+            for col in 0..<nm[1] where paper[row][col] {
                 var queue = [(row, col)]
                 var idx = 0
                 var width = 1
-                paper[row][col] = 0
+                paper[row][col] = false
 
                 while idx < queue.count {
                     let (x, y) = queue[idx]
                     idx += 1
 
                     [(x-1, y), (x, y-1), (x, y+1), (x+1, y)].forEach {
-                        if 0..<nm[0] ~= $0.0 && 0..<nm[1] ~= $0.1 && paper[$0.0][$0.1] == 1 {
+                        if 0..<nm[0] ~= $0.0 && 0..<nm[1] ~= $0.1 && paper[$0.0][$0.1] {
                             width += 1
-                            paper[$0.0][$0.1] = 0
+                            paper[$0.0][$0.1] = false
                             queue.append($0)
                         }
                     }
