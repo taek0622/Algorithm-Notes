4070fd041bcd96ae27ece3a77051a3de7325a882
taek0622 <min4644@gmail.com>
2023-12-27 12:30:53 +0900

    BOJ5525: IOIOI
    - 문자열

---
+++ Swift/Swift-Algorithm/BOJ/5525.swift
@@ -0,0 +1,40 @@
+//
+//  5525.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2023/02/04.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/5525
+//  알고리즘 분류: 문자열
+
+class BOJ5525: Solvable {
+    func run() {
+        // 메모리: 85716KB, 시간: 128ms, 코드 길이: 368B
+        let N = Int(readLine()!)!
+        let M = Int(readLine()!)!
+        let S = Array(readLine()!)
+        var count = 0
+        var result = 0
+        var idx = 0
+
+        while idx < M-2 {
+            if S[idx...idx+2] == ["I", "O", "I"] {
+                count += 1
+                idx += 2
+
+                if count == N {
+                    result += 1
+                    count -= 1
+                }
+
+                continue
+            }
+
+            count = 0
+            idx += 1
+        }
+
+        print(result)
+    }
+}
