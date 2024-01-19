fdbdc8c444833085014bc26db1bb442e2fef54da
taek0622 <min4644@gmail.com>
2024-01-19 19:55:26 +0900

    BOJ12919: A와 B 2
    - 구현, 브루트포스 알고리즘, 재귀

---
+++ Swift/Swift-Algorithm/BOJ/12919.swift
@@ -0,0 +1,48 @@
+//
+//  12919.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 1/19/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/12919
+//  알고리즘 분류: 구현, 브루트포스 알고리즘, 재귀
+
+class BOJ12919: Solvable {
+    func run() {
+        // 메모리: 69096KB, 시간: 8ms, 코드 길이: 472B
+        let S = readLine()!
+        let T = readLine()!
+        var isAvailable = false
+
+        dfs(T)
+
+        print(isAvailable ? 1 : 0)
+
+        func dfs(_ t: String) {
+            if isAvailable {
+                return
+            }
+
+            if S.count == t.count {
+                if S == t {
+                    isAvailable = true
+                }
+
+                return
+            }
+
+            if t.last == "A" {
+                var t = t
+                t.removeLast()
+                dfs(t)
+            }
+
+            if t.first == "B" {
+                var t = String(t.reversed())
+                t.removeLast()
+                dfs(t)
+            }
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ14725()
+let main = BOJ12919()
 main.run()
