1e7eca3dce362d93caf9e690e7ef9a63e3b6d1dc
taek0622 <min4644@gmail.com>
2024-01-30 19:24:25 +0900

    BOJ2437: 저울
    - 그리디 알고리즘, 정렬

---
+++ Swift/Swift-Algorithm/BOJ/2437.swift
@@ -0,0 +1,28 @@
+//
+//  2437.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 1/30/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/2437
+//  알고리즘 분류: 그리디 알고리즘, 정렬
+
+class BOJ2437: Solvable {
+    func run() {
+        // 메모리: 69108KB, 시간: 8ms, 코드 길이: 218B
+        let N = Int(readLine()!)!
+        let weights = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+        var sum = 1
+
+        for idx in 0..<N {
+            if weights[idx] > sum {
+                break
+            }
+
+            sum += weights[idx]
+        }
+
+        print(sum)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ11000()
+let main = BOJ2437()
 main.run()
