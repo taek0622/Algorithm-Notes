a292bcba23abd896738b13577cc644f29302760f
taek0622 <min4644@gmail.com>
2024-02-03 15:07:34 +0900

    BOJ1092: 배
    - 그리디 알고리즘, 정렬

---
+++ Swift/Swift-Algorithm/BOJ/1092.swift
@@ -0,0 +1,47 @@
+//
+//  1092.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2/3/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/1092
+//  알고리즘 분류: 그리디 알고리즘, 정렬
+
+class BOJ1092: Solvable {
+    func run() {
+        // 메모리: 70100KB, 시간: 28ms, 코드 길이: 744B
+        let N = Int(readLine()!)!
+        var cranes = readLine()!.split(separator: " ").map { Int($0)! }.sorted(by: >)
+        let M = Int(readLine()!)!
+        var boxes = readLine()!.split(separator: " ").map { Int($0)! }.sorted(by: >)
+
+        while !cranes.isEmpty && cranes.last! < boxes.last! {
+            cranes.removeLast()
+        }
+
+        if cranes.isEmpty || cranes.first! < boxes.first! {
+            print(-1)
+        } else {
+            var time = 0
+
+            while !boxes.isEmpty {
+                for craneIdx in cranes.indices {
+                    var boxIdx = 0
+
+                    while boxIdx < boxes.count && cranes[craneIdx] < boxes[boxIdx] {
+                        boxIdx += 1
+                    }
+
+                    if boxIdx < boxes.count {
+                        boxes.remove(at: boxIdx)
+                    }
+                }
+
+                time += 1
+            }
+
+            print(time)
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ2212()
+let main = BOJ1092()
 main.run()
