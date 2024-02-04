b9e15941fd7e2c57f7e9eaac6741ce4f53d18dba
taek0622 <min4644@gmail.com>
2024-02-04 23:56:53 +0900

    BOJ5585: 거스름돈
    - 그리디 알고리즘

---
+++ Swift/Swift-Algorithm/BOJ/5585.swift
@@ -0,0 +1,24 @@
+//
+//  5585.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2/4/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/5585
+//  알고리즘 분류: 그리디 알고리즘
+
+class BOJ5585: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 152B
+        var price = 1000 - Int(readLine()!)!
+        var count = 0
+
+        for yen in [500, 100, 50, 10, 5] {
+            count += price / yen
+            price %= yen
+        }
+
+        print(price + count)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ1092()
+let main = BOJ5585()
 main.run()
