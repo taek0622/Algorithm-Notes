e8870d5871a02759f9d02745f0e60aaef0055ef3
taek0622 <min4644@gmail.com>
2024-02-06 19:16:13 +0900

    BOJ10775: 공항
    - 자료 구조, 그리디 알고리즘, 분리 집합

---
+++ Swift/Swift-Algorithm/BOJ/10775.swift
@@ -0,0 +1,41 @@
+//
+//  10775.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2/6/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/10775
+//  알고리즘 분류: 자료 구조, 그리디 알고리즘, 분리 집합
+
+class BOJ10775: Solvable {
+    func run() {
+        // 메모리: 74448KB, 시간: 64ms, 코드 길이: 390B
+        let G = Int(readLine()!)!
+        let P = Int(readLine()!)!
+        var gates = Array(0...G)
+        var count = 0
+
+        for _ in 0..<P {
+            let gate = find(Int(readLine()!)!)
+
+            if gate == 0 {
+                break
+            }
+
+            gates[gate] -= 1
+            count += 1
+        }
+
+        print(count)
+
+        func find(_ gate: Int) -> Int {
+            if gates[gate] == gate {
+                return gate
+            }
+
+            gates[gate] = find(gates[gate])
+            return gates[gate]
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ3109()
+let main = BOJ10775()
 main.run()
