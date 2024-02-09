2c4ae2945ecb94d0782a63c6bacad98849e9c6f4
taek0622 <min4644@gmail.com>
2024-02-09 18:55:50 +0900

    BOJ2022: 사다리
    - 수학, 기하학, 이분 탐색, 피타고라스 정리

---
+++ Swift/Swift-Algorithm/BOJ/2022.swift
@@ -0,0 +1,33 @@
+//
+//  2022.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 2/9/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/2022
+//  알고리즘 분류: 수학, 기하학, 이분 탐색, 피타고라스 정리
+
+import Foundation
+
+class BOJ2022: Solvable {
+    func run() {
+        // 메모리: 79508KB, 시간: 12ms, 코드 길이: 443B
+        let xyc = readLine()!.split(separator: " ").map { Double($0)! }
+        var (left, right) = (0.0, min(xyc[0], xyc[1]))
+
+        while (right - left) >= 0.001 {
+            let width = (left + right) / 2
+            let h1 = sqrt(xyc[0] * xyc[0] - width * width)
+            let h2 = sqrt(xyc[1] * xyc[1] - width * width)
+
+            if (h1 * h2) / (h1 + h2) >= xyc[2] {
+                left = width
+            } else {
+                right = width
+            }
+        }
+
+        print(String(format: "%.3f", right))
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,5 +16,5 @@
 
 import Foundation
 
-let main = BOJ17386()
+let main = BOJ2022()
 main.run()
