e060fc07f63a776ac8f829c16370b2ad230a5009
taek0622 <min4644@gmail.com>
2024-04-16 14:10:16 +0900

    BOJ2412: 암벽 등반
    - 자료 구조, 그래프 이론, 그래프 탐색, 너비 우선 탐색, 해시를 사용한 집합과 맵

---
+++ Swift/Swift-Algorithm/BOJ/2412.swift
@@ -0,0 +1,47 @@
+//
+//  2412.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 4/16/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/2412
+//  알고리즘 분류: 자료 구조, 그래프 이론, 그래프 탐색, 너비 우선 탐색, 해시를 사용한 집합과 맵
+
+class BOJ2412: Solvable {
+    func run() {
+        // 메모리: 79736KB, 시간: 260ms, 코드 길이: 807B
+        let nT = readLine()!.split(separator: " ").map { Int($0)! }
+        var slots = [[Int]: Bool]()
+        var visited = slots
+        var count = -1
+
+        for _ in 0..<nT[0] {
+            let input = readLine()!.split(separator: " ").map { Int($0)! }
+            slots[[input[1], input[0]], default: false] = true
+        }
+
+        var queue = [(0, 0, 0)]
+        var idx = 0
+        visited[[0, 0], default: false] = true
+
+        while idx < queue.count {
+            let (y, x, move) = queue[idx]
+            idx += 1
+
+            if y == nT[1] {
+                count = move
+                break
+            }
+
+            for row in -2...2 {
+                for col in -2...2 where 0...nT[1] ~= y+row && 0...1000000 ~= x+col && slots[[y+row, x+col], default: false] && !visited[[y+row, x+col], default: false] {
+                    visited[[y+row, x+col], default: false] = true
+                    queue.append((y+row, x+col, move+1))
+                }
+            }
+        }
+
+        print(count)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ11085()
+let main = BOJ2412()
 main.run()
 
 class BOJ19538: Solvable {
