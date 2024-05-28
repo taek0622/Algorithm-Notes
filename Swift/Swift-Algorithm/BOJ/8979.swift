478e0a9b604a6891a2ab16836549617fb88287c0
taek0622 <min4644@gmail.com>
2024-05-28 18:27:00 +0900

    BOJ8979: 올림픽
    - 구현, 정렬

---
+++ Swift/Swift-Algorithm/BOJ/8979.swift
@@ -0,0 +1,31 @@
+//
+//  8979.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 5/28/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/8979
+//  알고리즘 분류: 구현, 정렬
+
+class BOJ8979: Solvable {
+    func run() {
+        // 메모리: 69112KB, 시간: 12ms, 코드 길이: 415B
+        let NK = readLine()!.split(separator: " ").map { Int($0)! }
+        var medal = (0, 0, 0)
+        var nations = [(Int, Int, Int)]()
+
+        for _ in 0..<NK[0] {
+            let input = readLine()!.split(separator: " ").map { Int($0)! }
+            nations.append((input[1], input[2], input[3]))
+
+            if input[0] == NK[1] {
+                medal = (input[1], input[2], input[3])
+            }
+        }
+
+        nations.sort(by: >)
+
+        print(nations.firstIndex(where: { $0 == medal })! + 1)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ1051()
+let main = BOJ8979()
 main.run()
 
 class BOJ19538: Solvable {
