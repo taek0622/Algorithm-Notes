4df4f28ee000848ece1d3d130f4dcedb1b426747
taek0622 <min4644@gmail.com>
2024-05-31 17:39:55 +0900

    BOJ1652: 누울 자리를 찾아라
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/1652.swift
@@ -0,0 +1,68 @@
+//
+//  1652.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 5/31/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/1652
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ1652: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 978B
+        let N = Int(readLine()!)!
+        var room = Array(repeating: Array(repeating: false, count: N), count: N)
+        var (countRow, countCol) = (0, 0)
+
+        for row in 0..<N {
+            room[row] = readLine()!.map { $0 == "." }
+        }
+
+        for row in 0..<N {
+            var tempCount = 0
+
+            for col in 0..<N {
+                if room[row][col] {
+                    tempCount += 1
+                } else {
+                    if tempCount >= 2 {
+                        countRow += 1
+                    }
+
+                    tempCount = 0
+                }
+
+                if col == N - 1 {
+                    if tempCount >= 2 {
+                        countRow += 1
+                    }
+                }
+            }
+        }
+
+        for col in 0..<N {
+            var tempCount = 0
+
+            for row in 0..<N {
+                if room[row][col] {
+                    tempCount += 1
+                } else {
+                    if tempCount >= 2 {
+                        countCol += 1
+                    }
+
+                    tempCount = 0
+                }
+
+                if row == N - 1 {
+                    if tempCount >= 2 {
+                        countCol += 1
+                    }
+                }
+            }
+        }
+
+        print("\(countRow) \(countCol)")
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ5635()
+let main = BOJ1652()
 main.run()
 
 class BOJ19538: Solvable {
