39aaa4640ce0046bc53f6d46721e236e87cbd279
taek0622 <min4644@gmail.com>
2024-06-02 17:02:08 +0900

    BOJ2440: 별 찍기 - 3
    - 구현

---
+++ Swift/Swift-Algorithm/BOJ/2440.swift
@@ -0,0 +1,20 @@
+//
+//  2440.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 6/2/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/2437
+//  알고리즘 분류: 구현
+
+class BOJ2440: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 118B
+        var N = Int(readLine()!)!
+
+        for idx in stride(from: N, to: 0, by: -1) {
+            print(String(repeating: "*", count: idx))
+        }
+    }
+}
