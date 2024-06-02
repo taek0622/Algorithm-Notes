c4140188a343448f44f536af32daf087a79c7abb
taek0622 <min4644@gmail.com>
2024-06-02 17:18:33 +0900

    BOJ11719: 그대로 출력하기 2
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/11719.swift
@@ -0,0 +1,18 @@
+//
+//  11719.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 6/2/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/11719
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ11719: Solvable {
+    func run() {
+        // 메모리: 69092KB, 시간: 8ms, 코드 길이: 49B
+        while let input = readLine() {
+            print(input)
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ2442()
+let main = BOJ11719()
 main.run()
 
 class BOJ19538: Solvable {
