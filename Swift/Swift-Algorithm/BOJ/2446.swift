d963951db19d472d5ee0f336eff13b0655aea324
taek0622 <min4644@gmail.com>
2024-06-02 17:23:25 +0900

    BOJ2446: 별 찍기 - 9
    - 구현

---
+++ Swift/Swift-Algorithm/BOJ/2446.swift
@@ -0,0 +1,24 @@
+//
+//  2446.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 6/2/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/2446
+//  알고리즘 분류: 구현
+
+class BOJ2446: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 12ms, 코드 길이: 272B
+        let N = Int(readLine()!)!
+
+        for idx in stride(from: N, to: 1, by: -1) {
+            print(String(repeating: " ", count: N-idx) + String(repeating: "*", count: idx*2-1))
+        }
+
+        for idx in 1...N {
+            print(String(repeating: " ", count: N-idx) + String(repeating: "*", count: idx*2-1))
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ11719()
+let main = BOJ2446()
 main.run()
 
 class BOJ19538: Solvable {
