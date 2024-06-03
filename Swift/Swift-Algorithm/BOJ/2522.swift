7129e10b1aa66b73d8397fd77914303ac79d2747
taek0622 <min4644@gmail.com>
2024-06-03 18:48:00 +0900

    BOJ2522: 별 찍기 - 12
    - 구현

---
+++ Swift/Swift-Algorithm/BOJ/2522.swift
@@ -0,0 +1,24 @@
+//
+//  2522.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 6/3/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/2522
+//  알고리즘 분류: 구현
+
+class BOJ2522: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 12ms, 코드 길이: 252B
+        let N = Int(readLine()!)!
+
+        for idx in 1..<N {
+            print(String(repeating: " ", count: N-idx) + String(repeating: "*", count: idx))
+        }
+
+        for idx in (1...N).reversed() {
+            print(String(repeating: " ", count: N-idx) + String(repeating: "*", count: idx))
+        }
+    }
+}
