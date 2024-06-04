821e1db7fa197446ebdc53b93f2c1300d8a882ab
taek0622 <min4644@gmail.com>
2024-06-04 17:19:29 +0900

    BOJ2752: 세수정렬
    - 구현, 정렬

---
+++ Swift/Swift-Algorithm/BOJ/2752.swift
@@ -0,0 +1,16 @@
+//
+//  2752.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 6/4/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/2744
+//  알고리즘 분류: 구현, 정렬
+
+class BOJ2752: Solvable {
+    func run() {
+        // 메모리: 69108KB, 시간: 8ms, 코드 길이: 108B
+        print(readLine()!.split(separator: " ").map { Int($0)! }.sorted().map { String($0) }.joined(separator: " "))
+    }
+}
