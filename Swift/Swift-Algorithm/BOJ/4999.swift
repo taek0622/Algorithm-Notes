bc18b6c27899c3c6f7cba8497c1ceb952521953b
taek0622 <min4644@gmail.com>
2024-06-07 18:06:42 +0900

    BOJ4999: 아!
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/4999.swift
@@ -0,0 +1,18 @@
+//
+//  4999.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 6/7/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/4963
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ4999: Solvable {
+    func run() {
+        // 메모리: 69092KB, 시간: 8ms, 코드 길이: 89B
+        let aaah = readLine()!.count
+        let aah = readLine()!.count
+        print(aaah >= aah ? "go" : "no")
+    }
+}
