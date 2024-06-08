0768da7f9cf3d103dcdc5cf6239cc5d13398a6eb
taek0622 <min4644@gmail.com>
2024-06-08 18:29:06 +0900

    BOJ10992: 별 찍기 - 17
    - 구현

---
+++ Swift/Swift-Algorithm/BOJ/10992.swift
@@ -0,0 +1,26 @@
+//
+//  10992.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 6/8/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/10992
+//  알고리즘 분류: 구현
+
+class BOJ10992: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 324B
+        let N = Int(readLine()!)!
+
+        for idx in 1...N {
+            if idx == 1 {
+                print(String(repeating: " ", count: N-idx) + "*")
+            } else if idx == N {
+                print(String(repeating: "*", count: N*2-1))
+            } else {
+                print(String(repeating: " ", count: N-idx) + "*" + String(repeating: " ", count: idx*2-3) + "*")
+            }
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ10996()
+let main = BOJ10992()
 main.run()
 
 class BOJ19538: Solvable {
