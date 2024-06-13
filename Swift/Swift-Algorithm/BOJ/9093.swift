eb3272cdc747e00c927aae996ecc0330414c3002
taek0622 <min4644@gmail.com>
2024-06-13 16:45:55 +0900

    BOJ9093: 단어 뒤집기
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/9093.swift
@@ -0,0 +1,21 @@
+//
+//  9093.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 6/13/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/9093
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ9093: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 224ms, 코드 길이: 165B
+        let T = Int(readLine()!)!
+
+        for _ in 0..<T {
+            let input = readLine()!.split(separator: " ").map { String($0.reversed()) }
+            print(input.joined(separator: " "))
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ17608()
+let main = BOJ9093()
 main.run()
 
 class BOJ19538: Solvable {
