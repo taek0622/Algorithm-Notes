9a4eaeb511ca82e0d6f00f5e41a99e6de639cb10
taek0622 <min4644@gmail.com>
2024-06-15 18:43:09 +0900

    BOJ10995: 별 찍기 - 20
    - 구현

---
+++ Swift/Swift-Algorithm/BOJ/10995.swift
@@ -0,0 +1,20 @@
+//
+//  10995.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 6/15/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/10995
+//  알고리즘 분류: 구현
+
+class BOJ10995: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 120B
+        let N = Int(readLine()!)!
+
+        for idx in 1...N {
+            print((idx % 2 == 1 ? "" : " ") + String(repeating: "* ", count: N))
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ2851()
+let main = BOJ10995()
 main.run()
 
 class BOJ19538: Solvable {
