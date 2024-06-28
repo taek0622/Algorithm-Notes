d0db7e0ff06ef523e552a82b7a6901e18d7a4f6a
taek0622 <min4644@gmail.com>
2024-06-28 11:09:00 +0900

    BOJ2789: 유학 금지
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/2789.swift
@@ -0,0 +1,16 @@
+//
+//  2789.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 6/28/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/2789
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ2789: Solvable {
+    func run() {
+        // 메모리: 69096KB, 시간: 8ms, 코드 길이: 55B
+        print(readLine()!.filter { !"CAMBRIDGE".contains($0) })
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ4458()
+let main = BOJ2789()
 main.run()
 
 class BOJ19538: Solvable {
