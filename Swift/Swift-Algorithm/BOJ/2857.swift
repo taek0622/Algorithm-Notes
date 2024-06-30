d57bda2283f9bc4304fe31ef7648d7f182dcb84f
taek0622 <min4644@gmail.com>
2024-06-30 17:09:50 +0900

    BOJ2857: FBI
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/2857.swift
@@ -0,0 +1,24 @@
+//
+//  2857.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 6/30/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/2857
+//  알고리즘 분류: 구현, 문자열
+
+import Foundation
+
+class BOJ2857: Solvable {
+    func run() {
+        // 메모리: 76504KB, 시간: 12ms, 코드 길이: 161B
+        var result = ""
+
+        for idx in 1...5 where readLine()!.contains("FBI") {
+            result += "\(idx) "
+        }
+
+        print(result == "" ? "HE GOT AWAY!" : result)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ2810()
+let main = BOJ2857()
 main.run()
 
 class BOJ19538: Solvable {
