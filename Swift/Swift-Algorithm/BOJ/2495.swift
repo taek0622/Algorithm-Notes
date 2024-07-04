dd29b79e973647d711749836ff8f1b59f6fb85e2
taek0622 <min4644@gmail.com>
2024-07-04 21:27:58 +0900

    BOJ2495: 연속구간
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/2495.swift
@@ -0,0 +1,33 @@
+//
+//  2495.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 7/4/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/2495
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ2495: Solvable {
+    func run() {
+        // 메모리: 69096KB, 시간: 8ms, 코드 길이: 345B
+        for _ in 0..<3 {
+            let input = Array(readLine()!)
+            var (count, maxCount) = (1, 1)
+
+            for idx in 1..<8 {
+                if input[idx-1] == input[idx] {
+                    count += 1
+                } else {
+                    if count > maxCount {
+                        maxCount = count
+                    }
+
+                    count = 1
+                }
+            }
+
+            print(max(count, maxCount))
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ2947()
+let main = BOJ2495()
 main.run()
 
 class BOJ19538: Solvable {
