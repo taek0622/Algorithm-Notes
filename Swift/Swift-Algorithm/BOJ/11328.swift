4f4bb920fab97be3283230d02a8f96778133e801
taek0622 <min4644@gmail.com>
2024-07-05 19:40:03 +0900

    BOJ11328: Strfry
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/11328.swift
@@ -0,0 +1,31 @@
+//
+//  11328.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 7/5/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/11328
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ11328: Solvable {
+    func run() {
+        // 메모리: 69108KB, 시간: 92ms, 코드 길이: 409B
+        let N = Int(readLine()!)!
+
+        for _ in 0..<N {
+            let strings = readLine()!.split(separator: " ").map { String($0) }
+            var chars = Array(repeating: 0, count: 26)
+
+            for char in strings[0] {
+                chars[Int(char.asciiValue!) - 97] += 1
+            }
+
+            for char in strings[1] {
+                chars[Int(char.asciiValue!) - 97] -= 1
+            }
+
+            print(chars == Array(repeating: 0, count: 26) ? "Possible" : "Impossible")
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ2495()
+let main = BOJ11328()
 main.run()
 
 class BOJ19538: Solvable {
