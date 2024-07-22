a40cec935bad610aa9906ff668a8dcea5a0a32a2
taek0622 <min4644@gmail.com>
2024-07-22 23:17:48 +0900

    BOJ11944: NN
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/11944.swift
@@ -0,0 +1,29 @@
+//
+//  11944.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 7/22/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/11944
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ11944: Solvable {
+    func run() {
+        // 메모리: 69108KB, 시간: 8ms, 코드 길이: 291B
+        let NM = readLine()!.split(separator: " ").map { String($0) }
+        let (N, M) = (Int(NM[0])!, Int(NM[1])!)
+        var result = [Character]()
+
+        for _ in 0..<N {
+            result.append(contentsOf: NM[0])
+
+            if result.count > M {
+                result.removeSubrange(M...)
+                break
+            }
+        }
+
+        print(String(result))
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ1551()
+let main = BOJ11944()
 main.run()
 
 class BOJ19538: Solvable {
