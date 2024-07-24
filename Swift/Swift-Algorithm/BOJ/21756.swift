e0cdf28495abb9b07d22b3bbe3bee10c808efd44
taek0622 <min4644@gmail.com>
2024-07-24 17:51:15 +0900

    BOJ21756: 지우개
    - 수학, 구현, 시뮬레이션

---
+++ Swift/Swift-Algorithm/BOJ/21756.swift
@@ -0,0 +1,23 @@
+//
+//  21756.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 7/24/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/21756
+//  알고리즘 분류: 수학, 구현, 시뮬레이션
+
+class BOJ21756: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 86B
+        let N = Int(readLine()!)!
+        var now = 1
+
+        while now * 2 <= N {
+            now *= 2
+        }
+
+        print(now)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ2774()
+let main = BOJ21756()
 main.run()
 
 class BOJ19538: Solvable {
