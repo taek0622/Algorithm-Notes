1f80229cc1f9768be454a81f77207bc3b7d9d2e4
taek0622 <min4644@gmail.com>
2024-07-27 17:23:59 +0900

    BOJ1673: 치킨 쿠폰
    - 수학, 구현

---
+++ Swift/Swift-Algorithm/BOJ/1673.swift
@@ -0,0 +1,28 @@
+//
+//  1673.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 7/27/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/1673
+//  알고리즘 분류: 수학, 구현
+
+class BOJ1673: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 265B
+        while let input = readLine() {
+            var nk = input.split(separator: " ").map { Int($0)! }
+            var count = 0
+
+            while nk[0] >= nk[1] {
+                count += nk[0] / nk[1] * nk[1]
+                nk[0] = nk[0] % nk[1] + nk[0] / nk[1]
+            }
+
+            count += nk[0]
+
+            print(count)
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ6679()
+let main = BOJ1673()
 main.run()
 
 class BOJ19538: Solvable {
