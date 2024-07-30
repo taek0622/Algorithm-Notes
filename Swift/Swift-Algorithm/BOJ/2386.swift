198d8c7050f932983cc5b65e905a0771d9cde321
taek0622 <min4644@gmail.com>
2024-07-30 23:18:58 +0900

    BOJ2386: 도비의 영어 공부
    - 구현, 문자열, 브루트포스 알고리즘

---
+++ Swift/Swift-Algorithm/BOJ/2386.swift
@@ -0,0 +1,18 @@
+//
+//  2386.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 7/30/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/2386
+//  알고리즘 분류: 구현, 문자열, 브루트포스 알고리즘
+
+class BOJ2386: Solvable {
+    func run() {
+        // 메모리: 69096KB, 시간: 8ms, 코드 길이: 131B
+        while let input = readLine()?.lowercased(), input != "#" {
+            print(input.first!, input.filter { $0 == input.first! }.count - 1)
+        }
+    }
+}
