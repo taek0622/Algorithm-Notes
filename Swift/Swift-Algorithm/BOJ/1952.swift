730df26b65b67830645520c05f7354a31f0b3c78
taek0622 <min4644@gmail.com>
2024-08-10 21:30:18 +0900

    BOJ1952: 달팽이2
    - 수학, 구현, 시뮬레이션

---
+++ Swift/Swift-Algorithm/BOJ/1952.swift
@@ -0,0 +1,22 @@
+//
+//  1952.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 8/10/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/1952
+//  알고리즘 분류: 수학, 구현, 시뮬레이션
+
+class BOJ1952: Solvable {
+    func run() {
+        let MN = readLine()!.split(separator: " ").map { Int($0)! }
+        var result = MN.min()! * 2 - 1
+
+        if MN[0] <= MN[1] {
+            result -= 1
+        }
+
+        print(result)
+    }
+}
