f4e349db7e2e05f822665eeae1f8aa62b19a62b8
taek0622 <min4644@gmail.com>
2024-08-11 20:39:25 +0900

    BOJ22864: 피로도
    - 수학, 구현, 그리디 알고리즘, 사칙연산, 시뮬레이션

---
+++ Swift/Swift-Algorithm/BOJ/22864.swift
@@ -0,0 +1,33 @@
+//
+//  22864.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 8/11/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/22864
+//  알고리즘 분류: 수학, 구현, 그리디 알고리즘, 사칙연산, 시뮬레이션
+
+class BOJ22864: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 308B
+        let ABCM = readLine()!.split(separator: " ").map { Int($0)! }
+        var time = 24
+        var task = 0
+        var tired = 0
+
+        while time > 0 {
+            if tired + ABCM[0] > ABCM[3] {
+                tired -= ABCM[2]
+                tired = max(0, tired)
+            } else {
+                tired += ABCM[0]
+                task += ABCM[1]
+            }
+
+            time -= 1
+        }
+
+        print(task)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ1952()
+let main = BOJ22864()
 main.run()
 
 class BOJ19538: Solvable {
