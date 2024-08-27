b1675bc825706903332586ff55942da2b8efcf47
taek0622 <min4644@gmail.com>
2024-08-27 18:47:21 +0900

    BOJ17389: 보너스 점수
    - 구현

---
+++ Swift/Swift-Algorithm/BOJ/17389.swift
@@ -0,0 +1,29 @@
+//
+//  17389.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 8/27/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/17389
+//  알고리즘 분류: 구현
+
+class BOJ17389: Solvable {
+    func run() {
+        // 메모리: 69260KB, 시간: 8ms, 코드 길이: 230B
+        let N = Int(readLine()!)!
+        let S = Array(readLine()!)
+        var (score, bonus) = (0, 0)
+
+        for idx in 0..<N {
+            if S[idx] == "O" {
+                score += (idx + 1 + bonus)
+                bonus += 1
+            } else {
+                bonus = 0
+            }
+        }
+
+        print(score)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ3943()
+let main = BOJ17389()
 main.run()
 
 class BOJ19538: Solvable {
