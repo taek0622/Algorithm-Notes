2276257261d0f3ceeaf2698821eb76bea7ce41e6
taek0622 <min4644@gmail.com>
2024-09-01 19:53:57 +0900

    BOJ17249: 태보태보 총난타
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/17249.swift
@@ -0,0 +1,17 @@
+//
+//  17249.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 9/1/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/17249
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ17249: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 135B
+        let string = readLine()!.split(separator: "0")
+        print("\(string[0].filter { $0 == "@" }.count) \(string[1].filter { $0 == "@" }.count)")
+    }
+}
