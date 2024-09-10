9e6634bbc97f436a7970275feb1dd044765e707d
taek0622 <min4644@gmail.com>
2024-09-10 16:10:48 +0900

    BOJ10174: 팰린드롬
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/10174.swift
@@ -0,0 +1,21 @@
+//
+//  10174.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 9/10/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/10174
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ10174: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 146B
+        let n = Int(readLine()!)!
+
+        for _ in 0..<n {
+            let input = readLine()!.lowercased()
+            print(input == String(input.reversed()) ? "Yes" : "No")
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ27160()
+let main = BOJ10174()
 main.run()
 
 class BOJ19538: Solvable {
