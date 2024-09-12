2bb0b5aded84a90ae5c6ba7620381c4dfd67f506
taek0622 <min4644@gmail.com>
2024-09-12 15:16:31 +0900

    BOJ5363: 요다
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/5363.swift
@@ -0,0 +1,21 @@
+//
+//  5363.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 9/12/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/5363
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ5363: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 161B
+        let N = Int(readLine()!)!
+
+        for _ in 0..<N {
+            let input = readLine()!.split(separator: " ")
+            print(input[2...].joined(separator: " "), input[0], input[1])
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ14910()
+let main = BOJ5363()
 main.run()
 
 class BOJ19538: Solvable {
