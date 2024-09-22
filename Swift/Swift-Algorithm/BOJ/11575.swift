ede26bafcd80ca87f1099e402fa73a86bbd06edb
taek0622 <min4644@gmail.com>
2024-09-22 17:49:53 +0900

    BOJ11575: Affine Cipher
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/11575.swift
@@ -0,0 +1,22 @@
+//
+//  11575.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 9/22/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/11575
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ11575: Solvable {
+    func run() {
+        // 메모리: 88588KB, 시간: 1116ms, 코드 길이: 240B
+        let T = Int(readLine()!)!
+
+        for _ in 0..<T {
+            let ab = readLine()!.split(separator: " ").map { Int($0)! }
+            let s = readLine()!.map { String(UnicodeScalar((Int($0.asciiValue! - 65) * ab[0] + ab[1]) % 26 + 65)!) }
+            print(s.joined())
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ4592()
+let main = BOJ11575()
 main.run()
 
 class BOJ19538: Solvable {
