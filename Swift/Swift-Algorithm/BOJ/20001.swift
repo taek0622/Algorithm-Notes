ece94942ec20491640d684f74c0a270ab8c54668
taek0622 <min4644@gmail.com>
2024-10-05 12:26:21 +0900

    BOJ20001: 고무오리 디버깅
    - 구현

---
+++ Swift/Swift-Algorithm/BOJ/20001.swift
@@ -0,0 +1,30 @@
+//
+//  20001.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 10/5/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/20001
+//  알고리즘 분류: 구현
+
+class BOJ20001: Solvable {
+    func run() {
+        // 메모리: 69096KB, 시간: 8ms, 코드 길이: 346B
+        var problem = 0
+
+        while let input = readLine(), input != "고무오리 디버깅 끝" {
+            if input == "문제" {
+                problem += 1
+            } else if input == "고무오리" {
+                if problem > 0 {
+                    problem -= 1
+                } else {
+                    problem += 2
+                }
+            }
+        }
+
+        print(problem == 0 ? "고무오리야 사랑해" : "힝구")
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ7600()
+let main = BOJ20001()
 main.run()
 
 class BOJ19538: Solvable {
