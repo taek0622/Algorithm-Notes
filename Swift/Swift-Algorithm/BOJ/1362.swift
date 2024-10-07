cd4fa3aea2d78b7b8864a246fa624ffa8292efdd
taek0622 <min4644@gmail.com>
2024-10-07 21:38:27 +0900

    BOJ1362: 펫
    - 구현, 시뮬레이션

---
+++ Swift/Swift-Algorithm/BOJ/1362.swift
@@ -0,0 +1,54 @@
+//
+//  1362.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 10/7/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/1362
+//  알고리즘 분류: 구현, 시뮬레이션
+
+class BOJ1362: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 822B
+        var idx = 1
+        var start = true
+        var weight = (0.0, 0.0)
+        var status = ""
+
+        while let input = readLine(), input != "0 0" {
+            let effect = input.split(separator: " ")
+
+            if start {
+                weight = (Double(effect[0])!, Double(effect[1])!)
+                start = false
+                continue
+            }
+
+            switch effect[0] {
+                case "#":
+                    if status != "RIP" {
+                        status = weight.1 > weight.0 * 0.5 && weight.1 < weight.0 * 2 ? ":-)" : ":-("
+                    }
+
+                    print("\(idx) \(status)")
+                    idx += 1
+                    start = true
+                    status = ""
+                    continue
+                case "E":
+                    weight.1 -= Double(effect[1])!
+
+                    if weight.1 <= 0 {
+                        status = "RIP"
+                    }
+                case "F":
+                    if status != "RIP" {
+                        weight.1 += Double(effect[1])!
+                    }
+                default:
+                    break
+            }
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ12778()
+let main = BOJ1362()
 main.run()
 
 class BOJ19538: Solvable {
