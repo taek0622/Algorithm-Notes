c9408bdff4c4b6e410e7cf805ebdb8d11e205789
taek0622 <min4644@gmail.com>
2024-10-09 18:39:12 +0900

    BOJ9046: 복호화
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/9046.swift
@@ -0,0 +1,38 @@
+//
+//  9046.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 10/9/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/9046
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ9046: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 620B
+        let T = Int(readLine()!)!
+
+        for _ in 0..<T {
+            var alphabet = Array(repeating: 0, count: 26)
+            var eCandidate = [Int]()
+            var alphabetMaxCount = -1
+            let input = readLine()!
+
+            for char in input where char != " " {
+                alphabet[Int(char.asciiValue!) - 97] += 1
+            }
+
+            for idx in alphabet.indices {
+                if alphabet[idx] > alphabetMaxCount {
+                    alphabetMaxCount = alphabet[idx]
+                    eCandidate = [idx]
+                } else if alphabet[idx] == alphabetMaxCount {
+                    eCandidate.append(idx)
+                }
+            }
+
+            print(eCandidate.count == 1 ? UnicodeScalar(eCandidate[0] + 97)! : "?")
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ12780()
+let main = BOJ9046()
 main.run()
 
 class BOJ19538: Solvable {
