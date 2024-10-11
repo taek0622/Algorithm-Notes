7e7d7a8be781c9de296b7a7b7cc274f9383a1fca
taek0622 <min4644@gmail.com>
2024-10-11 14:13:36 +0900

    BOJ4597: 패리티
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/4597.swift
@@ -0,0 +1,27 @@
+//
+//  4597.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 10/11/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/4597
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ4597: Solvable {
+    func run() {
+        // 메모리: 69096KB, 시간: 8ms, 코드 길이: 326B
+        while let input = readLine(), input != "#" {
+            var string = Array(input)
+            let bitCount = string.filter { $0 == "1" }.count % 2 == 0
+
+            if string.last! == "e" {
+                string[string.count - 1] = bitCount ? "0" : "1"
+            } else {
+                string[string.count - 1] = bitCount ? "1" : "0"
+            }
+
+            print(String(string))
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ14726()
+let main = BOJ4597()
 main.run()
 
 class BOJ19538: Solvable {
