8f295f9c226fb29fece05371f0e2807830af166a
taek0622 <min4644@gmail.com>
2024-10-15 16:11:32 +0900

    BOJ21918: 전구
    - 구현, 시뮬레이션

---
+++ Swift/Swift-Algorithm/BOJ/21918.swift
@@ -0,0 +1,42 @@
+//
+//  21918.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 10/15/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/21918
+//  알고리즘 분류: 구현, 시뮬레이션
+
+class BOJ21918: Solvable {
+    func run() {
+        // 메모리: 69252KB, 시간: 68ms, 코드 길이: 726B
+        let NM = readLine()!.split(separator: " ").map { Int($0)! }
+        var bulbs = readLine()!.split(separator: " ").map { $0 == "1" }
+
+        for _ in 0..<NM[1] {
+            let abc = readLine()!.split(separator: " ").map { Int($0)! }
+
+            switch abc[0] {
+                case 1:
+                    bulbs[abc[1] - 1] = (abc[2] == 1)
+                case 2:
+                    for idx in abc[1]...abc[2] {
+                        bulbs[idx - 1].toggle()
+                    }
+                case 3:
+                    for idx in abc[1]...abc[2] {
+                        bulbs[idx - 1] = false
+                    }
+                case 4:
+                    for idx in abc[1]...abc[2] {
+                        bulbs[idx - 1] = true
+                    }
+                default:
+                    break
+            }
+        }
+
+        print(bulbs.map { $0 ? "1" : "0" }.joined(separator: " "))
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ15947()
+let main = BOJ21918()
 main.run()
 
 class BOJ19538: Solvable {
