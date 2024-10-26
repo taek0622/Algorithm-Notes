3c046895fd095a2055f9b72e7b5e731bd532d606
taek0622 <min4644@gmail.com>
2024-10-26 16:35:06 +0900

    BOJ16466: 콘서트
    - 구현, 정렬

---
+++ Swift/Swift-Algorithm/BOJ/16466.swift
@@ -0,0 +1,28 @@
+//
+//  16466.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 10/26/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/16466
+//  알고리즘 분류: 구현, 정렬
+
+class BOJ16466: Solvable {
+    func run() {
+        // 메모리: 133212KB, 시간: 372ms, 코드 길이: 243B
+        let _ = Int(readLine()!)!
+        var tickets = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+        var myTicket = 1
+
+        for ticket in tickets {
+            if ticket == myTicket {
+                myTicket += 1
+            } else {
+                break
+            }
+        }
+
+        print(myTicket)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ11319()
+let main = BOJ16466()
 main.run()
 
 class BOJ19538: Solvable {
