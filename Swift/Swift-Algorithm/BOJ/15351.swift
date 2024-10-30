412a2d436797471df5c9844c2302f0edfcbb474c
taek0622 <min4644@gmail.com>
2024-10-30 14:48:35 +0900

    BOJ15351: 인생 점수
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/15351.swift
@@ -0,0 +1,45 @@
+//
+//  15351.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 10/30/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/15351
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ15351: Solvable {
+    func run() {
+        solution1()
+    }
+
+    // 메모리: 69100KB, 시간: 8ms, 코드 길이: 305B
+    private func solution1() {
+        let N = Int(readLine()!)!
+
+        for _ in 0..<N {
+            let life = readLine()!
+            var totalScore = 0
+
+            for char in life {
+                let score = char.asciiValue!
+
+                if 65...90 ~= score {
+                    totalScore += Int(score - 64)
+                }
+            }
+
+            print(totalScore == 100 ? "PERFECT LIFE" : totalScore)
+        }
+    }
+
+    // 메모리: 69104KB, 시간: 8ms, 코드 길이: 192B
+    private func solution2() {
+        let N = Int(readLine()!)!
+
+        for _ in 0..<N {
+            let score = readLine()!.map { Int($0.asciiValue!) - 64 }.filter { 1...26 ~= $0 }.reduce(0, +)
+            print(score == 100 ? "PERFECT LIFE" : score)
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ3181()
+let main = BOJ15351()
 main.run()
 
 class BOJ19538: Solvable {
