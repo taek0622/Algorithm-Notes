a4dbbb8fca6a86f0368efa434257da0515160ce9
taek0622 <min4644@gmail.com>
2024-11-07 16:54:28 +0900

    BOJ4613: Quicksum
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/4613.swift
@@ -0,0 +1,25 @@
+//
+//  4613.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 11/7/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/4613
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ4613: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 228B
+        while let input = readLine(), input != "#" {
+            let packet = input.map { $0 == " " ? 0 : Int($0.asciiValue!) - 64 }
+            var sum = 0
+
+            for idx in packet.indices {
+                sum += (idx + 1) * packet[idx]
+            }
+
+            print(sum)
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ2703()
+let main = BOJ4613()
 main.run()
 
 class BOJ19538: Solvable {
