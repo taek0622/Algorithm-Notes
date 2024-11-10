320cca6c5ed78d3234c5db29a7460cbfcba4f87a
taek0622 <min4644@gmail.com>
2024-11-10 11:19:14 +0900

    BOJ3041: N-퍼즐
    - 구현

---
+++ Swift/Swift-Algorithm/BOJ/3041.swift
@@ -0,0 +1,30 @@
+//
+//  3041.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 11/10/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/3041
+//  알고리즘 분류: 구현
+
+class BOJ3041: Solvable {
+    func run() {
+        // 메모리: 69096KB, 시간: 8ms, 코드 길이: 352B
+        var result = 0
+
+        for row in 0..<4 {
+            let puzzle = Array(readLine()!)
+
+            for col in puzzle.indices where puzzle[col] != "." {
+                let num = Int(puzzle[col].asciiValue!) - 65
+                let (pr, pc) = (num / 4, num % 4)
+
+                result += row - pr > 0 ? row - pr : pr - row
+                result += col - pc > 0 ? col - pc : pc - col
+            }
+        }
+
+        print(result)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ17176()
+let main = BOJ3041()
 main.run()
 
 class BOJ19538: Solvable {
