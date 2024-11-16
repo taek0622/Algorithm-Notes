ee99f6f52c15fb78e5793d1e0c5703d093cf3373
taek0622 <min4644@gmail.com>
2024-11-16 23:04:07 +0900

    BOJ9226: 도깨비말
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/9226.swift
@@ -0,0 +1,25 @@
+//
+//  9226.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 11/16/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/9226
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ9226: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 330B
+        while let input = readLine(), input != "#" {
+            var word = input.map { String($0) }
+
+            for idx in word.indices where word[idx] == "a" || word[idx] == "e" || word[idx] == "i" || word[idx] == "o" || word[idx] == "u" {
+                word = Array(word[idx...]) + Array(word[..<idx])
+                break
+            }
+
+            print(word.joined() + "ay")
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ1417()
+let main = BOJ9226()
 main.run()
 
 class BOJ19538: Solvable {
