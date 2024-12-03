1831d57962580dd4c92a428ff725ad6402a51c0d
taek0622 <min4644@gmail.com>
2024-12-03 17:22:02 +0900

    BOJ16922: 로마 숫자 만들기
    - 수학, 구현, 브루트포스 알고리즘, 조합론, 백트래킹

---
+++ Swift/Swift-Algorithm/BOJ/16922.swift
@@ -0,0 +1,32 @@
+//
+//  16922.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 12/3/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/16922
+//  알고리즘 분류: 수학, 구현, 브루트포스 알고리즘, 조합론, 백트래킹
+
+class BOJ16922: Solvable {
+    func run() {
+        // 메모리: 69104KB, 시간: 8ms, 코드 길이: 301B
+        let N = Int(readLine()!)!
+        var numbers = Set([0])
+
+        for _ in 0..<N {
+            var next = Set<Int>()
+
+            for number in numbers {
+                next.insert(number + 1)
+                next.insert(number + 5)
+                next.insert(number + 10)
+                next.insert(number + 50)
+            }
+
+            numbers = next
+        }
+
+        print(numbers.count)
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ1268()
+let main = BOJ16922()
 main.run()
 
 class BOJ19238: Solvable {
