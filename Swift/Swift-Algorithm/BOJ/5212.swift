6a31ea1bc9b8cd9f5516a59ed3b22ac4fe7b8222
taek0622 <min4644@gmail.com>
2024-12-08 17:51:03 +0900

    BOJ5212: 지구 온난화
    - 구현, 시뮬레이션

---
+++ Swift/Swift-Algorithm/BOJ/5212.swift
@@ -0,0 +1,52 @@
+//
+//  5212.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 12/8/24.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/5212
+//  알고리즘 분류: 구현, 시뮬레이션
+
+class BOJ5212: Solvable {
+    func run() {
+        // 메모리: 69108KB, 시간: 8ms, 코드 길이: 964B
+        let RC = readLine()!.split(separator: " ").map { Int($0)! }
+        var map = Array(repeating: Array(repeating: ".", count: RC[1] + 2), count: RC[0] + 2)
+
+        for row in 1...RC[0] {
+            map[row] = ["."] + readLine()!.map { String($0) } + ["."]
+        }
+
+        var newMap = map
+
+        for row in 1...RC[0] {
+            for col in 1...RC[1] where map[row][col] == "X" {
+                var count = 0
+
+                for (nr, nc) in [(row-1, col), (row, col-1), (row, col+1), (row+1, col)] where 0...RC[0]+1 ~= nr && 0...RC[1]+1 ~= nc && map[nr][nc] == "." {
+                    count += 1
+                }
+
+                if count >= 3 {
+                    newMap[row][col] = "."
+                }
+            }
+        }
+
+        var (startR, startC, endR, endC) = (RC[0], RC[1], 0, 0)
+
+        for row in 1...RC[0] {
+            for col in 1...RC[1] where newMap[row][col] == "X" {
+                startR = min(startR, row)
+                endR = max(endR, row)
+                startC = min(startC, col)
+                endC = max(endC, col)
+            }
+        }
+
+        for row in startR...endR {
+            print(newMap[row][startC...endC].joined())
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ2399()
+let main = BOJ5212()
 main.run()
 
 class BOJ19238: Solvable {
