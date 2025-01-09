b8299747520c42e417671090241dbcf8cd1eb126
taek0622 <min4644@gmail.com>
2025-01-09 18:43:52 +0900

    BOJ11558: The Game of Death
    - 구현, 그래프 이론, 그래프 탐색, 시뮬레이션

---
+++ Swift/Swift-Algorithm/BOJ/11558.swift
@@ -0,0 +1,47 @@
+//
+//  11558.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 1/9/25.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/11558
+//  알고리즘 분류: 구현, 그래프 이론, 그래프 탐색, 시뮬레이션
+
+class BOJ11558: Solvable {
+    func run() {
+        // 메모리: 69312KB, 시간: 24ms, 코드 길이: 457B
+        let T = Int(readLine()!)!
+
+        for _ in 0..<T {
+            let N = Int(readLine()!)!
+            var nexts = [Int]()
+            var cycle = Array(repeating: false, count: N)
+
+            for _ in 0..<N {
+                nexts.append(Int(readLine()!)!)
+            }
+
+            var idx = 0
+            var K = 0
+
+            while !cycle[K] {
+                idx += 1
+
+                cycle[K] = true
+
+                K = nexts[K] - 1
+
+                if K + 1 == N {
+                    break
+                }
+            }
+
+            if K + 1 != N {
+                idx = 0
+            }
+
+            print(idx)
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ11292()
+let main = BOJ11558()
 main.run()
 
 class BOJ19238: Solvable {
