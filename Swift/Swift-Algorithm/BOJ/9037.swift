4e51c8b14bcfa83e34245e318d1f8318746c8dc3
taek0622 <min4644@gmail.com>
2025-01-10 17:30:47 +0900

    BOJ9037: The candy war
    - 구현, 시뮬레이션

---
+++ Swift/Swift-Algorithm/BOJ/9037.swift
@@ -0,0 +1,46 @@
+//
+//  9037.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 1/10/25.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/9037
+//  알고리즘 분류: 구현, 시뮬레이션
+
+class BOJ9037: Solvable {
+    func run() {
+        // 메모리: 69108KB, 시간: 8ms, 코드 길이: 590B
+        let T = Int(readLine()!)!
+
+        for _ in 0..<T {
+            let N = Int(readLine()!)!
+            var C = readLine()!.split(separator: " ").map { Int($0)! }
+            var count = 0
+
+            for idx in 0..<N where C[idx] % 2 != 0 {
+                C[idx] += 1
+            }
+
+            while Set(C).count != 1 {
+                var temp = Array(repeating: 0, count: N)
+
+                for idx in 0..<N {
+                    temp[idx] = C[idx] / 2
+                }
+
+                for idx in 0..<N {
+                    C[idx] = temp[idx] + temp[(idx+1) % N]
+
+                    if C[idx] % 2 != 0 {
+                        C[idx] += 1
+                    }
+                }
+
+                count += 1
+            }
+
+            print(count)
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ11558()
+let main = BOJ9037()
 main.run()
 
 class BOJ19238: Solvable {
