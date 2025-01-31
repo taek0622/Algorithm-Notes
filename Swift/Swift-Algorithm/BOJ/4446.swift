e9d3b2dbea0031e22c9ffbe2ddb6a4264ae77b42
taek0622 <min4644@gmail.com>
2025-01-31 17:40:18 +0900

    BOJ4446: ROT13
    - 구현, 문자열

---
+++ Swift/Swift-Algorithm/BOJ/4446.swift
@@ -0,0 +1,49 @@
+//
+//  4446.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 1/31/25.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/4446
+//  알고리즘 분류: 구현, 문자열
+
+class BOJ4446: Solvable {
+    func run() {
+        // 메모리: 69100KB, 시간: 8ms, 코드 길이: 1092B
+        while let input = readLine() {
+            let sentence = input.map { String($0) }
+            let lowerVowels = ["a", "i", "y", "e", "o", "u"]
+            let upperVowels = ["A", "I", "Y", "E", "O", "U"]
+            let lowerConsonants = ["b", "k", "x", "z", "n", "h", "d", "c", "w", "g", "p", "v", "j", "q", "t", "s", "r", "l", "m", "f"]
+            let upperConsonants = ["B", "K", "X", "Z", "N", "H", "D", "C", "W", "G", "P", "V", "J", "Q", "T", "S", "R", "L", "M", "F"]
+            var result = ""
+
+            for char in sentence {
+                if let idx = lowerVowels.firstIndex(of: char) {
+                    result += lowerVowels[(idx+3) % 6]
+                    continue
+                }
+
+                if let idx = upperVowels.firstIndex(of: char) {
+                    result += upperVowels[(idx+3) % 6]
+                    continue
+                }
+
+                if let idx = lowerConsonants.firstIndex(of: char) {
+                    result += lowerConsonants[(idx+10) % 20]
+                    continue
+                }
+
+                if let idx = upperConsonants.firstIndex(of: char) {
+                    result += upperConsonants[(idx+10) % 20]
+                    continue
+                }
+
+                result += char
+            }
+
+            print(result)
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ25178()
+let main = BOJ4446()
 main.run()
 
 class BOJ14584: Solvable {
