8ecccdb7127b02775e0428af8e1531421f6ca8e7
taek0622 <min4644@gmail.com>
2025-04-03 18:31:15 +0900

    BOJ28066: 타노스는 요세푸스가 밉다
    - 구현, 자료 구조, 시뮬레이션, 덱, 큐

---
+++ Swift/Swift-Algorithm/BOJ/28066.swift
@@ -0,0 +1,57 @@
+//
+//  28066.swift
+//  Swift-Algorithm
+//
+//  Created by 김민택 on 4/3/25.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/28066
+//  알고리즘 분류: 구현, 자료 구조, 시뮬레이션, 덱, 큐
+
+class BOJ28066: Solvable {
+    func run() {
+        // 메모리: 85252KB, 시간: 84ms, 코드 길이: 812B
+        let NK = readLine()!.split(separator: " ").map { Int($0)! }
+        var queue = Queue<Int>()
+        var start = 1
+
+        for num in 1...NK[0] {
+            queue.enqueue(num)
+        }
+
+        while queue.count > 1 && queue.count >= NK[1] {
+            for _ in 0..<NK[1] {
+                _ = queue.dequeue()
+            }
+
+            queue.enqueue(start)
+            start = queue.first!
+        }
+
+        print(start)
+
+        struct Queue<T> {
+            private var inQueue = [T]()
+            private var outQueue = [T]()
+            var count: Int {
+                inQueue.count + outQueue.count
+            }
+            var first: T? {
+                outQueue.last ?? inQueue.first
+            }
+
+            mutating func enqueue(_ element: T) {
+                inQueue.append(element)
+            }
+
+            mutating func dequeue() -> T? {
+                if outQueue.isEmpty {
+                    outQueue = inQueue.reversed()
+                    inQueue.removeAll()
+                }
+
+                return outQueue.popLast()
+            }
+        }
+    }
+}
--- Swift/Swift-Algorithm/main.swift
+++ Swift/Swift-Algorithm/main.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-let main = BOJ17291()
+let main = BOJ28066()
 main.run()
 
 class BOJ14584: Solvable {
