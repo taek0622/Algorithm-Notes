7bfe6906e090a6c525ae187639cff1ab4ce4eaa5
taek0622 <min4644@gmail.com>
2025-08-19 14:59:03 +0900

    BOJ23806: 골뱅이 찍기 - ㅁ
    - 구현

--- Swift/App/Sources/main.swift
+++ Swift/App/Sources/main.swift
@@ -19,7 +19,7 @@ import Foundation
 import BOJ23000To23999
 import Shared
 
-let main = BOJ23802()
+let main = BOJ23806()
 main.run()
 
 public struct BOJ10253: Solvable {
---
+++ Swift/BOJ/BOJ23000To23999/Sources/23806.swift
@@ -0,0 +1,32 @@
+//
+//  23806.swift
+//  BOJ23000To23999
+//
+//  Created by 김민택 on 8/19/25.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/23806
+//  알고리즘 분류: 구현
+
+import Shared
+
+public struct BOJ23806: Solvable {
+    public init() {}
+
+    public func run() {
+        // 메모리: 69100KB, 시간: 16ms, 코드 길이: 295B
+        let N = Int(readLine()!)!
+
+        for _ in 0..<N {
+            print(String(repeating: "@", count: N*5))
+        }
+
+        for _ in 0..<N*3 {
+            print(String(repeating: "@", count: N) + String(repeating: " ", count: N*3) + String(repeating: "@", count: N))
+        }
+
+        for _ in 0..<N {
+            print(String(repeating: "@", count: N*5))
+        }
+    }
+}
