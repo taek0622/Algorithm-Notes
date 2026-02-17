54d954533fb252c7d461a09a94528385d6d17cb3
taek0622 <min4644@gmail.com>
2026-02-17 17:25:24 +0900

    BOJ27621: Sum of Three Cubes
    - 구현, 런타임 전의 전처리

--- Swift/App/Sources/main.swift
+++ Swift/App/Sources/main.swift
@@ -16,10 +16,10 @@
 
 import Foundation
 
-import BOJ07000To07999
+import BOJ27000To27999
 import Shared
 
-let main = BOJ7583()
+let main = BOJ27621()
 main.run()
 
 public struct BOJ11296: Solvable {
