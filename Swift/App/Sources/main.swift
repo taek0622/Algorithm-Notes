1f5b459c70334cf764e425d58109085d22c89535
taek0622 <min4644@gmail.com>
2026-04-15 22:45:58 +0900

    BOJ2110: 공유기 설치
    - 이분 탐색, 매개 변수 탐색

--- Swift/App/Sources/main.swift
+++ Swift/App/Sources/main.swift
@@ -16,11 +16,53 @@
 
 import Foundation
 
-import BOJ17000To17999
+//import BOJ02000To02999
 import Shared
 
-let main = BOJ17069()
+let main = BOJ2110()
 main.run()
+
+public struct BOJ2110: Solvable {
+    public init() {}
+
+    public func run() {
+        // 메모리: KB, 시간: ms, 코드 길이: 604B
+        let NC = readLine()!.split(separator: " ").map { Int($0)! }
+        var house = Array(repeating: 0, count: NC[0])
+
+        for idx in 0..<NC[0] {
+            house[idx] = Int(readLine()!)!
+        }
+
+        house.sort()
+
+        var left = 1
+        var right = house[NC[0]-1] - house[0]
+
+        while left <= right {
+            let mid = (left + right) / 2
+            var prev = house[0]
+            var count = 1
+
+            for idx in 1..<NC[0] where house[idx] - prev >= mid {
+                prev = house[idx]
+                count += 1
+
+                if count >= NC[1] {
+                    break
+                }
+            }
+
+            if count >= NC[1] {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+
+        print(right)
+    }
+}
 
 public struct BOJ11296: Solvable {
     public init() {}
